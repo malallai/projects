c69cdb9b0570b82e96e993d635f1ca1448a09df9
Malo ALLAIN <malallai@e1r12p20.42.fr>
2018-11-10 17:25:03 +0100

    strstr

--- libft/ft_strstr.c
+++ libft/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 15:40:03 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/10 15:41:46 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/10 17:24:57 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,5 @@ char	*ft_strstr(const char *haystack, const char *needle)
 {
 	if (!needle)
 		return (haystack);
+	
 }
