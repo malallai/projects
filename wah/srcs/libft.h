de8ea1b4d8ceb867d8a4ae30a33adf3922db6946
mentor6561 <melimalo@live.fr>
2018-11-10 09:34:45 +0100

    jgope

--- wah/srcs/libft.h
+++ wah/srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 11:52:48 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/08 11:52:48 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/08 16:45:13 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 # include <stdlib.h>
 # include <string.h>
 
-void	*
+void	*ft_memeset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *s, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strdup(char *src);
 
 #endif
