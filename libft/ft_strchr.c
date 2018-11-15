1dbe71f993ff0ab95ec17eca6a773985a4990ee6
mentor6561 <melimalo@live.fr>
2018-11-15 13:08:13 +0100

    ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,31 +6,11 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 15:20:13 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/15 12:17:04 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/15 13:08:06 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	*fts_strchr(const char *s, int c)
-{
-	char	ch;
-	char	*tmp;
-	int		i;
-
-	ch = (unsigned char)c;
-	i = 0;
-	tmp = (char *)s;
-	while (tmp[i])
-	{
-		if (tmp[i] == ch)
-			return (&tmp[i]);
-		i++;
-	}
-	if (!tmp[i] && !ch)
-		return (&tmp[i]);
-	return (NULL);
-}
 
 char	*ft_strchr(const char *s, int c)
 {
