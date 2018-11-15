bb398710ef5c056b487873240bde1a1a66cba90a
Malo ALLAIN <malallai@e1r4p17.42.fr>
2018-11-15 17:37:07 +0100

    ejopge

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 18:02:22 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/15 16:32:02 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/15 17:36:09 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,26 @@
 
 char	*ft_itoa(int n)
 {
-	(void)n;
-	return (NULL);
+	char	*result;
+	int		temp;
+	size_t	i;
+	int		r;
+	int		index;
+
+	i = (n < 0 ? 1 : 0);
+	temp = ft_abs(n);
+	while (temp /= 10 != 0)
+		i++;
+	index = i - 1;
+	result = ft_strnew(i);
+	if (n < 0)
+		result[0] = '-';
+	while (temp != 0)
+	{
+		r = temp % 10;
+		result[index--] = r + 48;
+		temp /= 10;
+	}
+	result[i] = '\0';
+	return (result);
 }
---
+++ libft/main.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+
+int main (void)
+{
+    printf("%s\n", ft_itoa(-154));
+    printf("%s\n", ft_itoa(154));
+    printf("%s\n", ft_itoa(-647));
+    printf("%s\n", ft_itoa(578422));
+    return (0);
+}
