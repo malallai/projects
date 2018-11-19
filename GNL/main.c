b48bbcd12090ef165faa30a04e8a7c66aced76c2
mentor6561 <melimalo@live.fr>
2018-11-19 20:03:42 +0100

    jopge

--- GNL/main.c
+++ GNL/main.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 13:16:40 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/19 19:26:31 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/19 20:03:34 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int main(int argc, char **argv)
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
+		get_next_line(fd, &line);
+		free(line);
 		get_next_line(fd, &line);
 	}
 	return (0);
