b45fb4bf528256846fc874831cc946231a63bb96
malallai <malallai@student.42.fr>
2018-11-19 20:05:43 +0100

    jopge

--- GNL/main.c
+++ GNL/main.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 13:16:40 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/19 20:04:21 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/19 20:05:39 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 	{
 		fd = open(argv[1], O_RDONLY);
 		get_next_line(fd, &line);
-		//free(line);
+		free(line);
 		//get_next_line(fd, &line);
 	}
 	return (0);
