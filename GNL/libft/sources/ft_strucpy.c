44a3902676ef6f9e49751482d1a4606e44857291
malallai <malallai@student.42.fr>
2018-11-19 19:27:03 +0100

    jopfe

---
+++ GNL/libft/sources/ft_strucpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strucpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/19 19:15:19 by malallai          #+#    #+#             */
+/*   Updated: 2018/11/19 19:15:19 by malallai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strccpy(char *dst, char *src, char c)
+{
+	size_t		i;
+	size_t		len;
+
+	if (!dst || !src)
+		return (NULL);
+	i = 0;
+	len = ft_strlen(src);
+	while (src[i] != c)
+	{
+		if (i < len)
+			dst[i] = src[i];
+		else
+			dst[i] = '\0';
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
