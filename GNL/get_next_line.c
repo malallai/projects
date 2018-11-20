f4c0ca935555ff360a6401eee807754e6d677e76
Malo ALLAIN <malallai@e1r11p19.42.fr>
2018-11-20 12:47:55 +0100

    gnl

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,15 +6,32 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 13:16:21 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/19 20:09:08 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/20 12:46:21 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <get_next_line.h>
 
-static int	ft_copyuntil(char **dst, char *src, char c)
+static t_list		*ft_getfile(t_list **files, int fd)
 {
-	int		i;
+	t_list *tmp;
+
+	tmp = *files;
+	while (tmp)
+	{
+		if ((int)tmp->content_size == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	tmp = ft_lstnew("\0", fd);
+	ft_lstadd(files, tmp);
+	tmp = *files;
+	return (tmp);
+}
+
+static size_t		ft_copyuntil(char **dst, char *src, char c)
+{
+	size_t		i;
 
 	i = -1;
 	while (src[++i])
@@ -26,24 +43,31 @@ static int	ft_copyuntil(char **dst, char *src, char c)
 	return (i);
 }
 
-int			get_next_line(const int fd, char **line)
+int					get_next_line(const int fd, char **line)
 {
 	char			buff[BUFF_SIZE];
-	//static t_list	file;
-	//int				i;
+	static t_list	*files;
 	int				r;
-	static char		*tmp;
+	t_list			*list;
+	size_t			cpyret;
 
+	if (fd < 0 || line == NULL || read(fd, buff, 0) < 0)
+		return (-1);
+	list = ft_getfile(&files, fd);
 	while ((r = read(fd, buff, BUFF_SIZE)))
 	{
 		buff[r] = '\0';
-		tmp = ft_strjoin(tmp, buff);
+		if (!(list->content = ft_strjoin(list->content, buff)))
+			return (-1);
 		if (ft_strchr(buff, '\n'))
-			break;
+			break ;
 	}
-	int size = ft_copyuntil(line, tmp, '\n');
-	printf("Line '%s'\n", *line);
-	tmp = tmp + size + 1;
-	printf("tmp '%s'\n", tmp);
+	if (r == 0)
+		return (0);
+	cpyret = ft_copyuntil(line, list->content, '\n');
+	if (cpyret < ft_strlen(list->content))
+		list->content = list->content + cpyret + 1;
+	else
+		ft_bzero(list->content, ft_strlen(list->content));
 	return (1);
 }
