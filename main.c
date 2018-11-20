54ec1491a2831cb34c5c00e9c2b18e06ebab8cae
Malo ALLAIN <malallai@e1r11p19.42.fr>
2018-11-20 12:47:55 +0100

    gnl

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 13:16:40 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/19 20:09:40 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/20 12:39:25 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ int main(int argc, char **argv)
 {
 	int		fd;
 	char	*line;
+	int		r;
 
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
-		get_next_line(fd, &line);
-		free(line);
-		get_next_line(fd, &line);
+		while ((r = get_next_line(fd, &line)) == 1)
+		{
+			ft_putendl(line);
+			free(line);
+		}
 		close(fd);
 	}
 	return (0);
