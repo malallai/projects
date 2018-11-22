b4eb4cae53ba5520bd6e4fa865aeef557824de61
Malo ALLAIN <malallai@e1r10p22.42.fr>
2018-11-22 17:20:27 +0100

    jgoehgopehgo

--- GNL/libft/sources/ft_strcpy.c
+++ GNL/libft/sources/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 16:41:17 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/22 16:53:20 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/22 16:56:24 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 char	*ft_strcpy(char *dest, const char *src)
 {
 	int i;
-	char tcpy;
-	char cpied;
 
 	if (!dest || !src)
 		return (NULL);
