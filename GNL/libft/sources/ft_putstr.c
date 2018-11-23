b4fd41f4d5974926f4dad1f49b86767bd260ca2c
Malo ALLAIN <malallai@e1r11p23.42.fr>
2018-11-23 16:42:21 +0100

    fjwp

--- GNL/libft/sources/ft_putstr.c
+++ GNL/libft/sources/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 12:34:46 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/20 12:49:38 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/23 16:09:04 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	ft_putstr(char const *str)
 {
 	if (str)
 	{
-		while (*str)
-			ft_putchar(*str++);
+		write(1, str, ft_strlen(str));
 	}
 }
