4ad2030264339dbb1342e12f8532d6d180e87364
mentor6561 <melimalo@live.fr>
2018-11-28 17:30:24 +0100

    fw;

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 13:18:36 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/28 15:03:19 by malallai         ###   ########.fr       */
+/*   Updated: 2018/11/28 17:30:04 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "libft/includes/libft.h"
 # include <fcntl.h>
 
-# define BUFF_SIZE 4096
+# define BUFF_SIZE 42
 
 int	get_next_line(const int fd, char **line);
 
