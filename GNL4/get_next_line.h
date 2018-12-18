f1962be2912d1954064ce5cc52598729842e22a6
mentor6561 <melimalo@live.fr>
2018-12-18 12:16:00 +0100

    w

--- GNL4/get_next_line.h
+++ GNL4/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 13:18:36 by malallai          #+#    #+#             */
-/*   Updated: 2018/12/18 11:40:31 by malallai         ###   ########.fr       */
+/*   Updated: 2018/12/18 12:00:06 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "libft/includes/libft.h"
 
 # define BUFF_SIZE 1024
+# define OPEN_MAX 10240
 
 int	get_next_line(const int fd, char **line);
 
