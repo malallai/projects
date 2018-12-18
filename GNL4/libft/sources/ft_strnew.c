08b2d3e8d8f9c704cb32c4fc53029ea6930ab48e
malallai <malallai@student.42.fr>
2018-12-18 11:08:00 +0100

    dw

--- GNL4/libft/sources/ft_strnew.c
+++ GNL4/libft/sources/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 17:22:24 by malallai          #+#    #+#             */
-/*   Updated: 2018/12/15 17:33:32 by malallai         ###   ########.fr       */
+/*   Updated: 2018/12/18 11:00:29 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,8 @@ char	*ft_strnew(size_t size)
 {
 	char *str;
 
-	printf("str new : %zu\n", size);
 	if (!(str = (char *)malloc(sizeof(char) * (size + 1))))
 		return (NULL);
-	printf("allocation work\n");
 	ft_bzero(str, size + 1);
-	printf("bzero work\n");
 	return (str);
 }
