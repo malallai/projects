6f1f83ce643cba7c8b9b57668676fc556404bbf5
root <root@localhost.localdomain>
2018-12-21 14:25:29 +0100

    dw

--- fillit/sources/fillit.c
+++ fillit/sources/fillit.c
@@ -1,23 +1,62 @@
 #include "../includes/fillit.h"
 
+int	is_char(char c)
+{
+	return (c  && (c == '.' || c == '#' || c == '\n'));
+}
+
+t_piece	read_buff(char *buffer)
+{
+	int	x;
+	int	y;
+	int	index;
+	t_piece piece;
+
+	x = 0;
+	y = 0;
+	index = 0;
+	piece = malloc(sizeof(t_piece));
+	while (index != 25)
+	{
+		if (is_char(buffer[index]))
+		{
+			if (buffer[index] == '#')
+			{
+				piece
+			}
+			index++;
+			x++;
+		}
+		else
+			return (NULL);
+		if (index % 5 == 0)
+		{
+			y++;
+			x = 0;
+			continue ;
+		}
+	}	
+}
+
 void	fillit(int argc, char ** argv)
 {
-	char *line;
-	int fd;
-	int r = 0 ;
+	char	buffer[25];
+	int	ret;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		if (!(fd = open(argv[1], O_RDONLY)))
+		ft_putendl("Usage: ./fillit [file]");
+		return ;
+	}
+	else
+	{
+		ret = read(argv[1], buffer, 24);
+		buffer[24] = '\0';
+		if (ret == -1)
 		{
-			ft_putendl("Error");
+			ft_putendl("error");
 			return ;
 		}
-		while ((r = ft_get_next_line(fd, &line)) > 0)
-		{
-			ft_putendl(line);
-			free(line);
-		}
-		close(fd);
+
 	}
 }
