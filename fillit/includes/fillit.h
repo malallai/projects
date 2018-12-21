6c919202198640d4888980680cca48aa5d09aded
root <root@localhost.localdomain>
2018-12-21 14:25:29 +0100

    dw

--- fillit/includes/fillit.h
+++ fillit/includes/fillit.h
@@ -2,6 +2,14 @@
 # define FILLIT_H
 # include "../libft/includes/libft.h"
 # include <fcntl.h>
+
+typedef struct	s_piece
+{
+	int	x;
+	int	y;
+	int	type;
+}		t_piece;
+
 void	fillit(int argc, char **argv);
 
 #endif
