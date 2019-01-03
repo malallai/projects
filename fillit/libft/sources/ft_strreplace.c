dfeb0f041e4abb978e240c9e179a525da16ce850
malallai <malallai@student.42.fr>
2019-01-03 14:27:22 +0100

    Debut

---
+++ fillit/libft/sources/ft_strreplace.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/10 15:40:03 by malallai          #+#    #+#             */
+/*   Updated: 2018/11/20 12:49:39 by malallai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strreplace(const char *str, char to_replace, char replace)
+{
+	char	*new_str;
+	int		str_index;
+	int		newstr_index;
+	char	c;
+
+	str_index = 0;
+	newstr_index = 0;
+	new_str = ft_strnew(ft_strlen(str));
+	while (str[str_index]) {
+		if (!replace && str[str_index] == to_replace)
+		{
+			str_index++;
+			continue ;
+		}
+		else
+		{
+			if (str[str_index] == to_replace)
+				c = replace;
+			else
+				c = str[str_index];
+		}
+		new_str[newstr_index] = c;
+		newstr_index++;
+		str_index++;
+	}
+	return (new_str);
+}
