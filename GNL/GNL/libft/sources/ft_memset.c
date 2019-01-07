5e8caff1a361a3e3b2ef30e603cd062cc17c6555
Malo ALLAIN <malallai@e1r10p20.42.fr>
2019-01-07 13:32:59 +0100

    w

---
+++ GNL/GNL/libft/sources/ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 12:29:59 by malallai          #+#    #+#             */
+/*   Updated: 2018/12/13 15:20:10 by malallai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *str, int a, size_t len)
+{
+	char *temp;
+
+	temp = str;
+	while (len--)
+		*temp[n] = (unsigned char)a;
+	return (str);
+}
