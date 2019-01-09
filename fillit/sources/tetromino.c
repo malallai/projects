82b0b9892fb4a013732db379974940dff17f656c
Malo ALLAIN <malallai@e1r13p14.42.fr>
2019-01-09 18:07:56 +0100

    jfw

--- fillit/sources/tetromino.c
+++ fillit/sources/tetromino.c
@@ -6,32 +6,11 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 16:22:31 by malallai          #+#    #+#             */
-/*   Updated: 2019/01/09 15:56:18 by malallai         ###   ########.fr       */
+/*   Updated: 2019/01/09 17:43:08 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
-
-char		**new_array(void)
-{
-	char	**new_array;
-	size_t	i;
-
-	i = 0;
-	if ((new_array = (char **)malloc(sizeof(char **) * 5)))
-	{
-		while (i < 4)
-		{
-			if (!(new_array[i] = ft_strnew(4)))
-				return (NULL);
-			i++;
-		}
-		new_array[i] = 0;
-	}
-	else
-		return (NULL);
-	return (new_array);
-}
 
 int			init_tetris(t_tetris **tetris, t_infos *infos)
 {
@@ -40,11 +19,12 @@ int			init_tetris(t_tetris **tetris, t_infos *infos)
 	if (!(tmp = (t_tetris *)malloc(sizeof(t_tetris))))
 		return (print_error(infos));
 	tmp->next = NULL;
-	if (!(tmp->array = new_array()))
+	if (!(tmp->array = ft_arraynew(5, 4)))
 		return (print_error(infos));
 	tmp->id = 0;
 	infos->last = tmp;
 	*tetris = tmp;
+	infos->tetris = tetris;
 	return (1);
 }
 
@@ -58,24 +38,10 @@ int			new_tetris(t_infos *infos)
 		return (print_error(infos));
 	tmp->next = NULL;
 	tmp->id = last->id + 1;
-	if (!(tmp->array = new_array()))
+	if (!(tmp->array = ft_arraynew(5, 4)))
 		return (print_error(infos));
 	last->next = tmp;
 	infos->last = tmp;
-	return (1);
-}
-
-int			check_tetro(t_tetris *tetris, t_infos *infos)
-{
-	size_t id;
-
-	if (!(tetris->charl = convert_to_array(tetris, 'A' + tetris->id)))
-		return (print_error(infos));
-	if (!(tetris->chard = convert_to_array(tetris, '#')))
-		return (print_error(infos));
-	id = ft_count_char(tetris->chard, '#');
-	if (id != 4)
-		return (print_error(infos));
 	return (1);
 }
 
@@ -103,4 +69,40 @@ char		*convert_to_array(t_tetris *tetris, char c)
 		i++;
 	}
 	return (tmp);
+}
+
+int			check_tetro(t_tetris *tetris, t_infos *infos)
+{
+	if (!(tetris->charl = convert_to_array(tetris, 'A' + tetris->id)))
+		return (print_error(infos));
+	if (!(tetris->chard = convert_to_array(tetris, '#')))
+		return (print_error(infos));
+	if (ft_count_char(tetris->chard, '#') != 4 || !check_connection(tetris))
+		return (print_error(infos));
+	return (1);
+}
+
+int			check_connection(t_tetris *tetris)
+{
+	int	block;
+	int	i;
+
+	block = 0;
+	i = 0;
+	while (i < 20)
+	{
+		if (tetris->chard[i] == '#')
+		{
+			if ((i + 1) < 20 && tetris->chard[i + 1] == '#')
+				block++;
+			if ((i - 1) >= 0 && tetris->chard[i - 1] == '#')
+				block++;
+			if ((i + 5) < 20 && tetris->chard[i + 5] == '#')
+				block++;
+			if ((i - 5) >= 0 && tetris->chard[i - 5] == '#')
+				block++;
+		}
+		i++;
+	}
+	return (block == 6 || block == 8);
 }
