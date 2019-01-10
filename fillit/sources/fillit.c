629753d46bfc6b88316091a00a07911335e22a1a
Malo ALLAIN <malallai@student.42.fr>
2019-01-10 16:46:14 +0100

    fjopaw

--- fillit/sources/fillit.c
+++ fillit/sources/fillit.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 20:53:22 by malallai          #+#    #+#             */
-/*   Updated: 2019/01/10 16:41:14 by malallai         ###   ########.fr       */
+/*   Updated: 2019/01/10 16:45:38 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,29 @@ int		fillit(t_infos *infos)
 		return (0);
 	print(infos->tetris);
 	return (1);
+}
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	t_infos *infos;
+
+	infos = NULL;
+	if (argc != 2)
+	{
+		ft_putendl("Usage: ./fillit [file]");
+		return (0);
+	}
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+			return (exit_fillit(infos, 1));
+		if (!(infos = new_infos(fd)))
+			return (exit_fillit(infos, 1));
+		if (!fillit(infos))
+			return (exit_fillit(infos, 1));
+		exit_fillit(infos, 0);
+	}
+	return (0);
 }
