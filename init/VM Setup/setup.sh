fccf2cf452e28805e8c0b52f83f8691fdaa911a3
root <root@vmdebian-malallai>
2019-01-18 01:00:58 +0100

    start init

---
+++ init/VM Setup/setup.sh
@@ -0,0 +1,14 @@
+apt-get update
+apt-get upgrade
+apt-get install -y sudo
+apt-get install -y htop
+apt-get install -y ssh
+apt-get install -y apache2
+apt-get install -y git
+apt-get install -y net-tools
+apt-get install -y ssmtp
+apt-get install -y mysql-server
+apt-get install -y mysql-client
+apt-get install -y phpmyadmin
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y man
