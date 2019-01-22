ff7ae00934295b34c1e7b35e196560e5bcce7f9b
malallai <malallai@student.42.fr>
2019-01-22 12:37:06 +0100

    BRYCE TRAVAILLE UN PEU PUTAIN

--- fillit/libft/ft_strsplit.c
+++ fillit/libft/ft_strsplit.c
@@ -5,113 +5,63 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/21 16:59:15 by malallai          #+#    #+#             */
-/*   Updated: 2019/01/21 17:48:34 by malallai         ###   ########.fr       */
+/*   Created: 2019/01/22 11:45:42 by malallai          #+#    #+#             */
+/*   Updated: 2019/01/22 12:26:30 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	count_words(const char *str, char splitter)
+static size_t	word_len(char const *s, char c)
 {
-	int		count;
-	char	*tmp;
-	int		index;
+	size_t i;
 
-	count = 0;
-	tmp = ft_strdup(str);
-	while (tmp[index])
-	{
-		if (tmp[index] == splitter)
-			index++;
-		else
-		{
-			count++;
-			while (tmp[index] && tmp[index] != splitter)
-				index++;
-		}
-	}
-	free(tmp);
-	return (count);
-}
-
-static char		**free_tab(char **array, size_t size)
-{
-	int index;
-
-	index = 0;
-	while (index != size)
-		free(array[index++]);
-	free(array);
-	return (NULL);
-}
-
-static char		*copy(char *str, char splitter)
-{
-	char	*tmp;
-	int		i;
-	int		size;
-
-	size = 0;
-	while (str[size] != splitter)
-		size++;
-	tmp = ft_strnew(size);
 	i = 0;
-	while (i < size)
-	{
-		tmp[i] = str[i];
+	while (s[i] && s[i] != c)
 		i++;
-	}
-	tmp[i] = '\0';
-	ft_putendl(tmp);
-	return (tmp);
+	return (i);
 }
 
-char			**ft_strsplit(char const *str, char splitter)
+static char		*next_word(char const *s, char c)
 {
-	char	**array;
-	char	*tmp_str;
-	int		index;
-	size_t	words;
-	char	*tmp;
+	while (*s && *s == c)
+		s++;
+	return ((char *)s);
+}
 
-	words = count_words(str, splitter);
-	if (!(array = malloc(sizeof(char *) * words)))
+static void		cleanup(char **split, size_t cur)
+{
+	while (cur > 0)
+	{
+		cur--;
+		ft_strdel(&split[cur]);
+	}
+	ft_strdel(split);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	char	**split;
+	size_t	cur;
+	size_t	wordcount;
+
+	wordcount = ft_countwords((char *)s, c);
+	split = (char **)ft_memalloc((wordcount + 1) * sizeof(char *));
+	if (split == NULL)
 		return (NULL);
-	tmp_str = ft_strdup(str);
-	index = 0;
-	while (index < words)
+	cur = 0;
+	while (cur < wordcount)
 	{
-		if (!(array[index] = copy(tmp_str, splitter)))
-			return (free_tab(array, words));
-		tmp = tmp_str;
-		tmp_str = ft_trimchar(tmp, splitter);
-		free(tmp);
-		index++;
+		s = next_word(s, c);
+		split[cur] = ft_strsub(s, 0, word_len(s, c));
+		if (split[cur] == NULL)
+		{
+			cleanup(split, cur);
+			return (NULL);
+		}
+		cur++;
+		s += word_len(s, c);
 	}
-	free(tmp_str);
-	array[index] = NULL;
-	index = 0;
-	while (index < words)
-	{
-		ft_putstr("Word : ");
-		ft_putendl(array[index++]);
-	}
-	return (array);
-}
-
-int	main(int argc, char **argv)
-{
-	char	**array;
-
-	if (argc > 1)
-		array = ft_strsplit(argv[1], *argv[2]);
-	int i = 0;
-	ft_putendl("test");
-	while (array[i])
-	{
-		ft_putstr("Word : ");
-		ft_putendl(array[i++]);
-	}
-	return (0);
+	split[wordcount] = NULL;
+	return (split);
 }
