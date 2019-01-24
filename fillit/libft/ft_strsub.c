97905e3952086d23a14cf7542c192d5fb6cc91ba
malallai <malallai@student.42.fr>
2019-01-24 18:35:16 +0100

    pus derreur

--- fillit/libft/ft_strsub.c
+++ fillit/libft/ft_strsub.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/12 11:54:56 by malallai          #+#    #+#             */
-/*   Updated: 2018/11/22 17:18:14 by malallai         ###   ########.fr       */
+/*   Created: 2015/12/02 04:09:51 by pbondoer          #+#    #+#             */
+/*   Updated: 2019/01/24 18:20:15 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	char *sub;
+	char	*new;
+	size_t	i;
 
-	if (!s || start + len > ft_strlen(s) || !(sub = ft_strnew(len)))
+	i = 0;
+	new = ft_strnew(len);
+	if (new == NULL)
 		return (NULL);
-	return (ft_strncpy(sub, s + start, len));
+	while (i < len)
+	{
+		new[i] = s[start + i];
+		i++;
+	}
+	return (new);
 }
