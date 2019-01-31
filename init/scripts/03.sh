954e22f4ab101d4c21259b8d6b6747de50737658
malallai <malallai@student.42.fr>
2019-01-31 15:36:05 +0100

    fwjopa

--- init/scripts/03.sh
+++ init/scripts/03.sh
@@ -5,9 +5,9 @@ GREEN="\033[38;5;70m"
 YELLOW="\033[38;5;3m"
 VERSION=0.0.1
 FOLDER=./
-ALL_FILE=1
+FILE=0
 
-while getopts "f:m:e:vh" flag
+while getopts "f:F:m:e:vh" flag
 do
 	case $flag in
 
@@ -15,23 +15,28 @@ do
 		printf "${GREEN}%s : %s\n" "Version" ${VERSION}
 		exit
 		;;
-	f)
+	F)
 		FOLDER=$OPTARG
+		;;
+	f)
+		FILE=1
+		INCLUDE=$OPTARG
 		;;
 	m)
 		MESSAGE=$OPTARG
 		;;
 	e)
-		ALL_FILE=0
+		FILE=2
 		EXCLUDE=$OPTARG
 		;;
 	h)
 		printf "%s -- :\n" "Help page"
 		printf "\t-%s : %s\n" "h" "Open help page"
 		printf "\t-%s : %s\n" "v" "Print current version"
-		printf "\t-%s : %s\n" "f" "Choose git repo folder"
+		printf "\t-%s : %s\n" "F" "Choose git repo folder"
+		printf "\t-%s : %s\n" "f" "File to include from commit (Seperate by ',')"
+		printf "\t-%s : %s\n" "e" "File to exclude from commit (Seperate by ',')"
 		printf "\t-%s : %s\n" "m" "Define commit message"
-		printf "\t-%s : %s\n" "e" "File to exclude from commit (Separate by ',')"
 		exit
 		;;
 	esac
@@ -39,21 +44,32 @@ done
 
 function add_files {
 	cd $FOLDER
-	if [ "$ALL_FILE" == "0" ]
+	if [ "$FILE" == "0" ]
 	then
 		git add .
+	else
 		OLDIFS=$IFS
 		IFS=','
-		read -ra array <<< "$EXCLUDE"
-		for arg in "${array[@]}"; do
-			git reset -- $arg
-		done
-		git status
+		if [ "$FILE" == "1" ]
+		then
+			read -ra array <<< "$INCLUDE"
+			for arg in "${array[@]}"; do
+				git add $arg >> /dev/null
+			done
+		else
+			read -ra array <<< "$EXCLUDE"
+			for arg in "${array[@]}"; do
+				git reset -- $arg >> /dev/null
+			done
+		fi
 		IFS=$OLDIFS
-	else
-		echo "nop"
 	fi
 }
 
+function commit_files {
+	echo "commited"
+}
+
 add_files
+commit_files
 
