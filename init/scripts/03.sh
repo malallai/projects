e9a8965a6111c75ed874d166ee20c53e65a1af22
malallai <malallai@student.42.fr>
2019-01-31 16:10:14 +0100

    2 files changed, 1 insertion(+), 1 deletion(-)

--- init/scripts/03.sh
+++ init/scripts/03.sh
@@ -76,9 +76,9 @@ function commit_files {
 }
 
 function push_files {
-	git push
 	BRANCH=`git branch | cut -c3-`
 	DIFF=`git diff --stat --cached origin/master | tail -1 | cut -d ' ' -f2`
+	git push
 	echo $DIFF
 	printf "${YELLOW}Pushed ${GREEN}%s ${YELLOW}files for branch ${GREEN}%s\n" "$DIFF" "$BRANCH"
 }
