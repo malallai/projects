a5d3a79891c545877b9f9a73bbe25de5ad7a603c
BuildTools <unconfigured@null.spigotmc.org>
2019-02-25 14:45:04 +0100

    Update

--- roger-skyline1/scripts/init.sh
+++ roger-skyline1/scripts/init.sh
@@ -9,6 +9,7 @@ function install_default {
 	apt-get install -y dnsutils
 	apt-get install -y iptables-persistent
 	apt-get install -y sendmail
+	apt-get install -y mailutils
 	apt-get install -y fail2ban
 }
 
