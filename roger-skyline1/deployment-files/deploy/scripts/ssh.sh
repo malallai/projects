3d64ea95cfd1ae415775b0fa78b80027a4a499e7
malallai <malallai@student.42.fr>
2019-03-11 00:29:38 +0100

    2 files changed, 3 insertions(+), 3 deletions(-)

--- roger-skyline1/deployment-files/deploy/scripts/ssh.sh
+++ roger-skyline1/deployment-files/deploy/scripts/ssh.sh
@@ -1,15 +1,16 @@
 function create_user {
 	useradd $ssh_user
 	mkdir /home/$ssh_user
-	chown $ssh_user:$ssh_user /home/$ssh_user
 }
 
 function init_ssh {
+	mkdir /home/$ssh_user/.ssh/
 	echo "../id_rsa.pub" >> /home/$ssh_user/.ssh/authorized_keys
 	sed -i "s|#Port 22|Port $ssh_port|" /etc/ssh/sshd_config
 	sed -i "s|#PermitRootLogin prohibit-password|PermitRootLogin no|" /etc/ssh/sshd_config
 	sed -i "s|#PubkeyAuthentication yes|PubkeyAuthentication yes|" /etc/ssh/sshd_config
 	sed -i "s|#PasswordAuthentication yes|PasswordAuthentication no|" /etc/ssh/sshd_config
+	chown -R $ssh_user:$ssh_user /home/$ssh_user
 	service sshd restart
 }
 
