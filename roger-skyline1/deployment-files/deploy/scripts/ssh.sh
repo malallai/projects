92ea2bddd90c7edae33a86619732dfb2993ed2d2
malallai <malallai@student.42.fr>
2019-03-10 22:28:37 +0100

    8 files changed, 96 insertions(+), 12 deletions(-)

--- roger-skyline1/deployment-files/deploy/scripts/ssh.sh
+++ roger-skyline1/deployment-files/deploy/scripts/ssh.sh
@@ -1,8 +1,13 @@
 function init_ssh {
-	echo ${idrsa} >> /home/${user}/.ssh/authorized_keys
+	echo ${ssh_pubkey} >> /home/${user}/.ssh/authorized_keys
 	sed -i 's/#Port 22/Port 2201/' /etc/ssh/sshd_config
 	sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
 	sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 	sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 	service sshd restart
+}
+
+function sudo_user {
+	usermod -aG sudo ${user}
+	echo "${user} ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
 }
