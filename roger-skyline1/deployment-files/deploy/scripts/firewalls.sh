7b53d36b77d888e6e7e950efe8ef32f15016d6f2
malallai <malallai@student.42.fr>
2019-03-12 13:31:46 +0100

    4 files changed, 28 insertions(+), 58 deletions(-)

--- roger-skyline1/deployment-files/deploy/scripts/firewalls.sh
+++ roger-skyline1/deployment-files/deploy/scripts/firewalls.sh
@@ -1,6 +1,12 @@
 function deploy_firewalls {
 	if [ "$firewalls_deploy" == "true" ]; then
-		/bin/bash ../files/iptables
+		#/bin/bash ../files/iptables
+		if ! [ "`ufw status | grep \"inactive\"" == "" ]; then
+			ufw enable
+		fi
+		ufw allow 2201/tcp
+		ufw allow 80/tcp
+		ufw allow 443
 		echo -e "${GREEN}Firewalls deployment finish."
 	fi
 }
