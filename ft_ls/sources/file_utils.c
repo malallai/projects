a26188d6af090fd1e5bbd7e91308e7200bbb4c81
malallai <malallai@student.42.fr>
2019-03-23 18:55:01 +0100

    20 files changed, 134 insertions(+), 107 deletions(-)

--- ft_ls/sources/file_utils.c
+++ ft_ls/sources/file_utils.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 15:42:50 by malallai          #+#    #+#             */
-/*   Updated: 2019/03/22 16:22:39 by malallai         ###   ########.fr       */
+/*   Updated: 2019/03/23 17:05:36 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		is_regular_file(const char *path)
 {
 	struct stat path_stat;
 
-	stat(path, &path_stat);
+	lstat(path, &path_stat);
 	return (S_ISREG(path_stat.st_mode));
 }
 
@@ -24,7 +24,7 @@ int		is_folder(const char *path)
 {
 	struct stat path_stat;
 
-	stat(path, &path_stat);
+	lstat(path, &path_stat);
 	return (S_ISDIR(path_stat.st_mode));
 }
 
@@ -43,4 +43,13 @@ char	*lsgetlink(char *path)
 	if ((link_len = readlink(path, buf, 1024)))
 		return (buf);
 	return (NULL);
+}
+
+t_file		*get_file(t_file *first, int id)
+{
+	if (!first || id < 0)
+		return (NULL);
+	if (first->id == id)
+		return (first);
+	return (get_file(first->next, id));
 }
--- ft_ls/sources/printer.c
+++ ft_ls/sources/printer.c
@@ -6,7 +6,7 @@
 /*   By: malallai <malallai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 15:46:40 by malallai          #+#    #+#             */
-/*   Updated: 2019/03/22 14:20:19 by malallai         ###   ########.fr       */
+/*   Updated: 2019/03/23 17:28:32 by malallai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,16 +38,16 @@ void	print_details(t_opt *opt, t_entry *entry, t_file *file, \
 	ft_putendl("");
 	if (has_flag(opt, F_BLOCKS))
 	{
-		put_nbr(infos->stat.st_blocks, 1, 0, entry->size.blocks);
+		put_nbr(infos->stat.st_blocks, 1, 0, entry->size->blocks);
 		put(infos->mode, 1, 1, 10);
 	}
 	else
 		put(infos->mode, 0, 0, 0);
-	put_nbr(infos->stat.st_nlink, 1, 2, entry->size.links);
-	put(infos->uid->pw_name, 1, 1, entry->size.uid);
-	put(infos->gid->gr_name, 1, 2, entry->size.gid);
-	put_nbr(infos->stat.st_size, 1, 2, entry->size.size);
-	put(file->date, 1, 1, entry->size.t);
+	put_nbr(infos->stat.st_nlink, 1, 2, entry->size->links);
+	put(infos->uid->pw_name, 1, 1, entry->size->uid);
+	put(infos->gid->gr_name, 1, 2, entry->size->gid);
+	put_nbr(infos->stat.st_size, 1, 2, entry->size->size);
+	put(file->date, 1, 1, entry->size->t);
 	ft_putstr(get_color(infos->stat.st_mode));
 	ft_putchar(' ');
 	put(infos->name, 0, 0, 0);
@@ -64,7 +64,7 @@ void	print_lnk(t_opt *opt, t_entry *entry, t_file *file, \
 	if (!S_ISLNK(infos->stat.st_mode))
 		return ;
 	ft_putstr(" -> ");
-	ft_putstr(lsgetlink(infos->full_path));
+	ft_putstr(lsgetlink(file->path));
 }
 
 void	put_nbr(int nbr, int tab, int spaces, int max)
