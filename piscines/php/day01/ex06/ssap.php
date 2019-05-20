2b5fbcfcc104921ac7483a6c26b33a6c6205ee14
malallai <malallai@student.42.fr>
2019-05-20 19:37:05 +0200

    11 files changed, 225 insertions(+), 147 deletions(-)

--- piscines/php/day01/ex06/ssap.php
+++ piscines/php/day01/ex06/ssap.php
@@ -1,16 +1,15 @@
 #!/usr/bin/php
 <?php
-    function ft_split($string) {
-        $array = array_filter(explode(' ', $string));
-        return ($array);
-    }
+	function ft_split($string) {
+		$array = array_filter(explode(' ', $string));
+		return ($array);
+	}
 
-    unset($argv[0]);
-    $array = array();
-    foreach($argv as $arg)
-        foreach(ft_split($arg) as $v)
-            array_push($array, $v);
-    sort($array);
-    foreach($array as $value)
-        echo $value."\n";
-?>
+	unset($argv[0]);
+	$array = array();
+	foreach ($argv as $arg)
+		foreach (ft_split($arg) as $v)
+			array_push($array, $v);
+	sort($array);
+	foreach ($array as $value)
+		echo $value."\n";
