91c11aff92243605d0f14650aa23f87b801efe8f
malallai <malallai@student.42.fr>
2019-05-23 19:16:27 +0200

    7 files changed, 136 insertions(+), 92 deletions(-)

--- piscines/php/day04/ex02/modif.php
+++ piscines/php/day04/ex02/modif.php
@@ -5,18 +5,24 @@
             $login = $_POST['login'];
             $old_passwd = hash('whirlpool', $_POST['oldpw']);
             $passwd = hash('whirlpool', $_POST['newpw']);
+            $exist = 0;
 
             foreach ($users as $key => $user) {
                 if ($user['login'] === $login) {
                     if ($old_passwd === $user['passwd']) {
                         $users[$key]['passwd'] = $passwd;
                         file_put_contents("../private/passwd", serialize($users));
-                        die ("OK\n");
-                    } else {
-                        die("ERROR\n");
+                        $exist = 1;
                     }
                 }
             }
+            if (!$exist)
+                echo "ERROR\n";
+            else
+                echo "OK\n";
+        } else {
+            echo "ERROR\n";
         }
+    } else {
+        echo "ERROR\n";
     }
-    die("ERROR\n");
--- piscines/php/day04/ex03/whoami.php
+++ piscines/php/day04/ex03/whoami.php
@@ -1,1 +1,6 @@
 <?php
+    session_start();
+    if ($_SESSION['loggued_on_user'])
+        echo $_SESSION['loggued_on_user']."\n";
+    else
+        echo "ERROR\n";
