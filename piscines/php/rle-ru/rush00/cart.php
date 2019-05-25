4ed6237e3532a3dcd063bc1cdecfd21a702b673e
malallai <malallai@student.42.fr>
2019-05-25 20:32:29 +0200

    2 files changed, 7 insertions(+), 7 deletions(-)

--- piscines/php/rle-ru/rush00/cart.php
+++ piscines/php/rle-ru/rush00/cart.php
@@ -4,13 +4,13 @@ include('templates/header.php');
     <div class="flex-items">
         <?php
         $file = unserialize(file_get_contents("../private/products"));
+        print_r($file);
         if ($file && isset($_SESSION['items'])) {
             foreach ($_SESSION['items'] as $key => $value) {
                 echo $key." : ".$value." => ";
                 foreach ($file as $item) {
                     if ($item['uid'] === $key) {
                         echo $item['name']." : ".$item['price']."\n";
-
                         break;
                     }
                 }
