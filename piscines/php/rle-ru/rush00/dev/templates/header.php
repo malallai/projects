f13f26457fe270e4b2a02645182e318d3fd1a236
malallai <malallai@student.42.fr>
2019-05-25 15:45:10 +0200

    9 files changed, 9 insertions(+), 9 deletions(-)

--- piscines/php/rle-ru/rush00/dev/templates/header.php
+++ piscines/php/rle-ru/rush00/dev/templates/header.php
@@ -6,20 +6,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="/dev/assets/css/style.css">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>ft_minishop</title>
 </head>
 <body>
 <div class="header">
     <div class="flex-compononent">
-        <img src="../assets/images/42.svg">
+        <img src="/assets/images/42.svg">
     </div>
     <div class="flex-compononent">
         <input name="search" placeholder="Search" type="text" class="searchbar">
     </div>
     <div class="flex-compononent">
-        <img src="../assets/images/42.svg">
+        <img src="/assets/images/42.svg">
     </div>
 </div>
 <div class="navbar">
