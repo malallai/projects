bf6c2971adbb2d41ed7a37360b1fcca7b6dab363
malallai <malallai@student.42.fr>
2019-05-25 20:26:31 +0200

    3 files changed, 74 insertions(+), 37 deletions(-)

--- piscines/php/rle-ru/rush00/additem.php
+++ piscines/php/rle-ru/rush00/additem.php
@@ -1,1 +1,17 @@
 <?php
+    session_start();
+
+    if (isset($_POST['submit']) && isset($_POST['id'])) {
+        $items = array();
+        if (isset($_SESSION['items'])) {
+            $items = $_SESSION['items'];
+        }
+        if (isset($items[$_POST['id']])) {
+            $items[$_POST['id']] = $items[$_POST['id']] + 1;
+        } else {
+            $items[$_POST['id']] = 1;
+        }
+        $_SESSION['items_count'] =  isset($_SESSION['items_count']) ? $_SESSION['items_count'] + 1 : 1;
+    }
+
+    header("Location: /");
