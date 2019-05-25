75db1a337ecaf37a8956f336fc3ccdb63a202253
malallai <malallai@student.42.fr>
2019-05-25 10:42:16 +0200

    47 files changed, 123 insertions(+), 12 deletions(-)

--- piscines/php/day04/ex04/login.php
+++ piscines/php/day04/ex04/login.php
@@ -1,1 +1,16 @@
 <?php
+    include 'auth.php';
+    session_start();
+
+    if ($_GET['login'] && $_GET['passwd']) {
+        if (auth($_GET['login'], $_GET['passwd'])) {
+            $_SESSION['loggued_on_user'] = $_GET['login'];
+        } else {
+            $_SESSION['loggued_on_user'] = "";
+            header("Location:./index.html");
+        }
+    } else {
+        header("Location:./index.html");
+    }
+?>
+
