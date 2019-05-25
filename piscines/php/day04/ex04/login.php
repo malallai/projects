2b28e1f24f808b238dec54a5eeea050264e4cc60
malallai <malallai@student.42.fr>
2019-05-25 11:22:09 +0200

    2 files changed, 25 insertions(+), 22 deletions(-)

--- piscines/php/day04/ex04/login.php
+++ piscines/php/day04/ex04/login.php
@@ -14,3 +14,13 @@
     }
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>42 chat</title>
+</head>
+<body>
+<h1>Hello World</h1>
+</body>
+</html>
