57403ff3cbbdd44c19b15399a5591003a28735c9
malallai <malallai@student.42.fr>
2019-05-25 11:37:29 +0200

    5 files changed, 120 insertions(+), 48 deletions(-)

--- piscines/php/day04/ex04/login.php
+++ piscines/php/day04/ex04/login.php
@@ -17,10 +17,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>42 chat</title>
 </head>
 <body>
-<h1>Hello World</h1>
+<li>
+    <iframe id="chat" name='chat' src='chat.php' width='100%' height='550'></iframe>
+    <iframe name='speak' src='speak.php' width='100%' height='50' scrolling='no'></iframe>
+</li>
+<a href='logout.php'>Se déconnecter</a>
+<a href='javascript:refresh();'>Rafraîchir</a>
+<script langage="javascript">
+    var frame = document.getElementById('chat');
+    frame.onload = function() {
+        frame.contentWindow.scrollTo(0, frame.contentWindow.document.getElementsByTagName('body')[0].scrollHeight);
+    }
+
+    function refresh () {
+        frame.src = 'chat.php';
+    }
+</script>
 </body>
 </html>
