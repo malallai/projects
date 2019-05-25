cb234ffd03c4a7c1df1f7730477217d196121fb0
malallai <malallai@student.42.fr>
2019-05-25 20:39:03 +0200

    2 files changed, 40 insertions(+), 26 deletions(-)

--- piscines/php/rle-ru/rush00/cart.php
+++ piscines/php/rle-ru/rush00/cart.php
@@ -4,14 +4,28 @@ include('templates/header.php');
     <div class="flex-items">
         <?php
         $file = unserialize(file_get_contents("../private/products"));
-        print_r($file);
-        print_r($_SESSION['items']);
         if ($file && isset($_SESSION['items'])) {
             foreach ($_SESSION['items'] as $key => $value) {
-                echo $key." : ".$value." => ";
                 foreach ($file as $item) {
                     if ($item['uid'] === $key) {
-                        echo $item['name']." : ".$item['price']."\n";
+                        ?>
+                        <div class="item">
+                            <div class="image">
+                                <img src="https://cdn.intra.42.fr/users/<?= $item['name'] ?>.jpg">
+                            </div>
+                            <div class="description">
+                                <div class="name">
+                                    <p><?= $item['name'] ?></p>
+                                </div>
+                                <div class="price">Prix : <?= $item['price'] ?>E (<?= ($value * $item['price']) ?>)E</div>
+                                <div class="quantity">Quantité : <?= $value ?></div>
+                                <form action="removeitem.php" method="post">
+                                    <input class="id" name="id" type="text" value="<?= $item['uid'] ?>">
+                                    <input class="submit" name="submit" type="submit" value="Supprimer">
+                                </form>
+                            </div>
+                        </div>
+                        <?php
                         break;
                     }
                 }
