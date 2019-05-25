5988214557f61bb6dc9386ba8edefe5cdbdfb54b
malallai <malallai@student.42.fr>
2019-05-25 19:42:41 +0200

    17 files changed, 491 insertions(+), 325 deletions(-)

---
+++ piscines/php/rle-ru/rush00/admin/index.php
@@ -0,0 +1,75 @@
+<?php
+include('templates/header.php');
+?>
+    <div class="flex-items">
+    <div class="item">
+        <div class="image">
+            <img src="https://cdn.intra.42.fr/users/malallai.jpg">
+        </div>
+        <div class="description">
+            <div class="name">
+                <p>nom</p>
+            </div>
+            <div class="price">Prix : x</div>
+            <div class="quantity">Quantité : x</div>
+            <form action="additem.php" method="post">
+                <input class="id" name="id" type="text" value="1">
+                <input class="submit" name="submit" type="submit" value="Ajouter au panier">
+            </form>
+        </div>
+    </div>
+    <div class="item">
+        <div class="image">
+            <img src="https://cdn.intra.42.fr/users/malallai.jpg">
+        </div>
+        <div class="description">
+            <div class="name">
+                <p>nom</p>
+            </div>
+            <div class="price">Prix : x</div>
+            <div class="quantity">Quantité : x</div>
+            <form action="additem.php" method="post">
+                <input class="id" name="id" type="text" value="1">
+                <input class="submit" name="submit" type="submit" value="Ajouter au panier">
+            </form>
+        </div>
+    </div>
+    <div class="item">
+        <div class="image">
+            <img src="https://cdn.intra.42.fr/users/malallai.jpg">
+        </div>
+        <div class="description">
+            <div class="name">
+                <p>nom</p>
+            </div>
+            <div class="price">Prix : x</div>
+            <div class="quantity">Quantité : x</div>
+            <form action="additem.php" method="post">
+                <input class="id" name="id" type="text" value="1">
+                <input class="submit" name="submit" type="submit" value="Ajouter au panier">
+            </form>
+        </div>
+    </div>
+    <html>
+    <head>
+        <link href="body.css" type="text/css" rel="stylesheet" />
+        <title>42Shop - Home</title>
+    </head>
+<body>
+<div id="main">
+    <?php
+    $file = unserialize(file_get_contents("../private/products"));
+    if ($file)
+    {
+        foreach($file as $f)
+        {
+            ?>
+            <img class="thu" src="<?php echo "https://cdn.intra.42.fr/users/".$f['name'].".jpg"?>" />
+            <?php
+        }
+    }
+    ?>
+</div>
+<?php
+include('templates/footer.php');
+?>
--- piscines/php/rle-ru/rush00/index.php
+++ piscines/php/rle-ru/rush00/index.php
@@ -2,73 +2,33 @@
 include('templates/header.php');
 ?>
 <div class="flex-items">
-    <div class="item">
-        <div class="image">
-            <img src="https://cdn.intra.42.fr/users/malallai.jpg">
-        </div>
-        <div class="description">
-            <div class="name">
-                <p>nom</p>
+    <?php
+    $file = unserialize(file_get_contents("../private/products"));
+    if ($file)
+    {
+        foreach($file as $f)
+        {
+            ?>
+            <div class="item">
+                <div class="image">
+                    <img src="https://cdn.intra.42.fr/users/<?= $f['name'] ?>.jpg">
+                </div>
+                <div class="description">
+                    <div class="name">
+                        <p><?= $f['name'] ?></p>
+                    </div>
+                    <div class="price">Prix : <?= $f['price'] ?>E</div>
+                    <div class="quantity">Quantité : <?= $f['price'] ?></div>
+                    <form action="additem.php" method="post">
+                        <input class="id" name="id" type="text" value="<?= $f['uid'] ?>">
+                        <input class="submit" name="submit" type="submit" value="Ajouter au panier">
+                    </form>
+                </div>
             </div>
-            <div class="price">Prix : x</div>
-            <div class="quantity">Quantité : x</div>
-            <form action="additem.php" method="post">
-                <input class="id" name="id" type="text" value="1">
-                <input class="submit" name="submit" type="submit" value="Ajouter au panier">
-            </form>
-        </div>
-    </div>
-    <div class="item">
-        <div class="image">
-            <img src="https://cdn.intra.42.fr/users/malallai.jpg">
-        </div>
-        <div class="description">
-            <div class="name">
-                <p>nom</p>
-            </div>
-            <div class="price">Prix : x</div>
-            <div class="quantity">Quantité : x</div>
-            <form action="additem.php" method="post">
-                <input class="id" name="id" type="text" value="1">
-                <input class="submit" name="submit" type="submit" value="Ajouter au panier">
-            </form>
-        </div>
-    </div>
-    <div class="item">
-        <div class="image">
-            <img src="https://cdn.intra.42.fr/users/malallai.jpg">
-        </div>
-        <div class="description">
-            <div class="name">
-                <p>nom</p>
-            </div>
-            <div class="price">Prix : x</div>
-            <div class="quantity">Quantité : x</div>
-            <form action="additem.php" method="post">
-                <input class="id" name="id" type="text" value="1">
-                <input class="submit" name="submit" type="submit" value="Ajouter au panier">
-            </form>
-        </div>
-    </div>
-<html>
-<head>
-	<link href="body.css" type="text/css" rel="stylesheet" />
-	<title>42Shop - Home</title>
-</head>
-<body>
-<div id="main">
-	<?php
-	$file = unserialize(file_get_contents("../private/products"));
-	if ($file)
-	{
-		foreach($file as $f)
-		{
-			?>
-			<img class="thu" src="<?php echo "https://cdn.intra.42.fr/users/".$f['name'].".jpg"?>" />
-			<?php
-		}
-	}
-	?>
+            <?php
+        }
+    }
+    ?>
 </div>
 <?php
 include('templates/footer.php');
