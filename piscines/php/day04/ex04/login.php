6f31fece95015951253823b1f8342082475975e8
malallai <malallai@student.42.fr>
2019-05-25 10:56:04 +0200

    2 files changed, 22 insertions(+), 9 deletions(-)

--- piscines/php/day04/ex04/login.php
+++ piscines/php/day04/ex04/login.php
@@ -5,6 +5,7 @@
     if ($_GET['login'] && $_GET['passwd']) {
         if (auth($_GET['login'], $_GET['passwd'])) {
             $_SESSION['loggued_on_user'] = $_GET['login'];
+            echo "OK\n";
         } else {
             $_SESSION['loggued_on_user'] = "";
             header("Location:./index.html");
