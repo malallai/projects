765e7a92433811b5c0fb49794a52086a460e89cf
malallai <malallai@student.42.fr>
2019-05-25 15:25:29 +0200

    8 files changed, 118 insertions(+), 52 deletions(-)

---
+++ piscines/php/rle-ru/rush00/dev/templates/header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Header file, contains the top of the front office (every pages).
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>ft_minishop</title>
+</head>
+<body>
+
