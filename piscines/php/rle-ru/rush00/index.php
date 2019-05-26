87295a9c3aa8978e23f715bd514f9ef64d2c178b
malallai <malallai@student.42.fr>
2019-05-26 17:32:49 +0200

    2 files changed, 16 insertions(+), 16 deletions(-)

--- piscines/php/rle-ru/rush00/index.php
+++ piscines/php/rle-ru/rush00/index.php
@@ -19,10 +19,10 @@ if (is_installed() !== true) {
     {
         $products = search_item_by_name($_GET['search']);
     }
-    print_r($_GET);
-    print_r(get_categories());
+    print_r(get_categories()['year']);
 
     foreach ($_GET as $key => $val) {
+        print_r(get_categories()[$key]);
         if (get_categories()[$key]) {
             $products = search_item_by_categories($products, $val);
         }
