da316a123c61013610a66cc5d31e8f46dea8669e
malallai <malallai@student.42.fr>
2019-05-26 15:47:25 +0200

    5 files changed, 37 insertions(+), 120 deletions(-)

--- piscines/php/rle-ru/rush00/register.php
+++ piscines/php/rle-ru/rush00/register.php
@@ -5,7 +5,6 @@ include('assets/templates/header.php');
         $new = array();
         $new['login'] = $_POST['login'];
         $new['pass'] = hash('whirlpool', $_POST['pass']);
-        $new['name'] = $_POST['name'];
         $new['grade'] = ($_POST['login'] == "rle-ru" || $_POST['login'] == "malallai") ? "admin" : "customer";
         $new['img'] = "https://cdn.intra.42.fr/users/".$new['login'].".jpg";
         if (create_user($new) !== true) {
@@ -23,7 +22,7 @@ include('assets/templates/header.php');
             <p>Création d'un compte</p>
             <div class="install">
                 <form method="POST" action="register.php">
-                    <input type="text" name="username" id="username" class="form-control" placeholder="Nom d'utilisateur">
+                    <input type="text" name="login" id="login" class="form-control" placeholder="Nom d'utilisateur">
                     <input type="password" name="password" id="password" class="form-control" placeholder="Mot de passe">
                     <input class="btn btn-white" name="create" value="Créer" type="submit">
                 </form>
