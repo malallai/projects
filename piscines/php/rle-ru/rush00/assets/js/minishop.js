125e10ea6c6b33f4d80cff57898d907f84ee3bb1
malallai <malallai@student.42.fr>
2019-05-26 18:54:50 +0200

    2 files changed, 8 insertions(+), 8 deletions(-)

--- piscines/php/rle-ru/rush00/assets/js/minishop.js
+++ piscines/php/rle-ru/rush00/assets/js/minishop.js
@@ -1,6 +1,6 @@
 function update_selected_categories(item, key) {
-    alert(item);
     var selectBox = document.getElementById("select-box");
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
-    document.location.href="index.php?"+key+"[]="+selectedValue;
+    console.log(item);
+    //document.location.href="index.php?"+key+"[]="+selectedValue;
 }
