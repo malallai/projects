0c6be407d64e8d2f59af0d00848637d92a1b5336
malallai <malallai@student.42.fr>
2019-05-26 17:44:54 +0200

    3 files changed, 50 insertions(+), 46 deletions(-)

--- piscines/php/rle-ru/rush00/index.php
+++ piscines/php/rle-ru/rush00/index.php
@@ -27,38 +27,45 @@ if (is_installed() !== true) {
     }
 	$page = isset($_GET['page']) && $_GET['page'] != NULL ? $_GET['page'] : 0;
 
-    if ($products)
-    {
-		$i = $page * 5;
-        foreach($products as $k=>$f)
-        {
-			if ($k >= $i && $k < $i + 5)
-			{
-            ?>
-            <div class="item">
-                <div class="image">
-                    <img src="https://cdn.intra.42.fr/users/<?= $f['name'] ?>.jpg">
-                </div>
-                <div class="description">
-                    <div class="name">
-                        <p><?= $f['name'] ?></p>
+    if ($products) {
+        $i = $page * 5;
+        foreach ($products as $k => $f) {
+            if ($k >= $i && $k < $i + 5) {
+                ?>
+                <div class="item">
+                    <div class="image">
+                        <img src="https://cdn.intra.42.fr/users/<?= $f['name'] ?>.jpg">
                     </div>
-                    <div class="price">Prix : <?= $f['price'] ?>E</div>
-                    <div class="quantity">Annee : <?= $f['categories']['year'] ?></div>
-                    <div class="quantity">Campus : <?= $f['categories']['campus'] ?></div>
-                    <form action="index.php" method="post">
-                        <input class="id" name="id" type="text" value="<?= $f['uid'] ?>">
-                        <input class="submit" name="submit" type="submit" value="Ajouter au panier">
-                    </form>
+                    <div class="description">
+                        <div class="name">
+                            <p><?= $f['name'] ?></p>
+                        </div>
+                        <div class="price">Prix : <?= $f['price'] ?>E</div>
+                        <div class="quantity">Annee : <?= $f['categories']['year'] ?></div>
+                        <div class="quantity">Campus : <?= $f['categories']['campus'] ?></div>
+                        <form action="index.php" method="post">
+                            <input class="id" name="id" type="text" value="<?= $f['uid'] ?>">
+                            <input class="submit" name="submit" type="submit" value="Ajouter au panier">
+                        </form>
+                    </div>
                 </div>
-            </div>
-			<?php
-			}
-		}
-		if ($page > 0)
-			echo "<a href=\"index.php?page=", $page - 1 , "\">Page precedente</a>";
-		if (count($products) > $i + 5)
-		    echo "<a href=\"index.php?page=", $page + 1 , "\">Page suivante</a>";
+                <?php
+            }
+        }
+        if ($page > 0) {
+            $request = "?page".($page - 1);
+            foreach ($_GET as $key => $value) {
+                $request .= "&".$key."=".$value;
+            }
+            echo "<a href=\"index.php".$request."\">Page precedente</a>";
+        }
+        if (count($products) > $i + 5) {
+            $request = "?page".($page + 1);
+            foreach ($_GET as $key => $value) {
+                $request .= "&".$key."=".$value;
+            }
+            echo "<a href=\"index.php".$request."\">Page suivante</a>";
+        }
     }
     ?>
 </div>
