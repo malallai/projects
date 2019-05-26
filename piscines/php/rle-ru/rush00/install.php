27a24cc71a25c1bfb86c9a4ac583e91ead8ec230
malallai <malallai@student.42.fr>
2019-05-26 13:30:49 +0200

    3 files changed, 62 insertions(+), 88 deletions(-)

--- piscines/php/rle-ru/rush00/install.php
+++ piscines/php/rle-ru/rush00/install.php
@@ -15,14 +15,17 @@
 <link rel="stylesheet" href="assets/css/install.css">
 <div class="container">
     <div class="install-row">
-        <div class="install">
-            <form method="POST" action="install.php">
-                <input type="text" name="username" id="username" class="form-control" placeholder="Nom d'utilisateur">
-                <input type="email" name="email" id="mail" class="form-control" placeholder="Adresse Email">
-                <input type="password" name="password" id="password" class="form-control" placeholder="Mot de passe">
-                <input type="password" name="repeat" id="repeatPass" class="form-control" placeholder="Répéter le mot de passe">
-                <input class="btn btn-green" name="create" value="Créer" type="submit">
-            </form>
+        <div class="install-content">
+            <p>Création du profile administrateur</p>
+            <div class="install">
+                <form method="POST" action="install.php">
+                    <input type="text" name="username" id="username" class="form-control" placeholder="Nom d'utilisateur">
+                    <input type="email" name="email" id="mail" class="form-control" placeholder="Adresse Email">
+                    <input type="password" name="password" id="password" class="form-control" placeholder="Mot de passe">
+                    <input type="password" name="repeat" id="repeatPass" class="form-control" placeholder="Répéter le mot de passe">
+                    <input class="btn btn-green" name="create" value="Créer" type="submit">
+                </form>
+            </div>
         </div>
     </div>
 </div>
