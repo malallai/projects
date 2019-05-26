4dcc638221ddd73a8da076b43deebfc70b047785
malallai <malallai@student.42.fr>
2019-05-26 17:58:29 +0200

    3 files changed, 124 insertions(+), 80 deletions(-)

--- piscines/php/rle-ru/rush00/index.php
+++ piscines/php/rle-ru/rush00/index.php
@@ -10,79 +10,100 @@ if (is_installed() !== true) {
     header("location: install.php");
 }
 ?>
-<div class="flex-items">
-
-    <?php
-    $products = get_products();
-
-    if ($_GET['search'])
-    {
-        $products = search_item_by_name($_GET['search']);
-    }
-
-    foreach ($_GET as $key => $val) {
-        if (get_categories()[$key]) {
-            $products = search_item_by_categories($products, array($key => $val));
-        }
-    }
-	$page = isset($_GET['page']) && $_GET['page'] != NULL ? $_GET['page'] : 0;
-
-    if ($products) {
-        $i = $page * 5;
-        foreach ($products as $k => $f) {
-            if ($k >= $i && $k < $i + 5) {
-                ?>
-                <div class="item">
-                    <div class="image">
-                        <img src="https://cdn.intra.42.fr/users/<?= $f['name'] ?>.jpg">
-                    </div>
-                    <div class="description">
-                        <div class="name">
-                            <p><?= $f['name'] ?></p>
-                        </div>
-                        <div class="price">Prix : <?= $f['price'] ?>E</div>
-                        <div class="quantity">Annee : <?= $f['categories']['year'] ?></div>
-                        <div class="quantity">Campus : <?= $f['categories']['campus'] ?></div>
-                        <form action="index.php" method="post">
-                            <input class="id" name="id" type="text" value="<?= $f['uid'] ?>">
-                            <input class="submit" name="submit" type="submit" value="Ajouter au panier">
-                        </form>
-                    </div>
-                </div>
-                <?php
-            }
-        }
-        if ($page > 0) {
-            $request = "?page=".($page - 1);
-            if (isset($_GET['page'])) unset($_GET['page']);
-            foreach ($_GET as $key => $value) {
-                if (is_array($value)) {
+    <div class="flex-categories">
+        <?php
+        $categories = get_categories();
+        foreach ($categories as $key => $value) {
+            ?>
+            <div class="category">
+                <select name="<?= $value ?>">
+                    <?php
                     foreach ($value as $key1 => $value1) {
-                        $request .= "&" . $key . "[]=" . $value1;
+                        ?>
+                            <option value="<?= $value1 ?>"><?= $value1 ?></option>
+                        <?php
                     }
-                } else {
-                    $request .= "&" . $key . "=" . $value;
-                }
-            }
-            echo "<a href=\"index.php".$request."\">Page precedente</a>";
+                    ?>
+                </select>
+            </div>
+            <?php
         }
-        if (count($products) > $i + 5) {
-            $request = "?page=".($page + 1);
-            if (isset($_GET['page'])) unset($_GET['page']);
-            foreach ($_GET as $key => $value) {
-                if (is_array($value)) {
-                    foreach ($value as $key1 => $value1) {
-                        $request .= "&" . $key . "[]=" . $value1;
-                    }
-                } else {
-                    $request .= "&" . $key . "=" . $value;
-                }
-            }
-            echo "<a href=\"index.php".$request."\">Page suivante</a>";
-        }
-    }
+
     ?>
-</div>
+    </div>
+    <div class="flex-items">
+
+        <?php
+        $products = get_products();
+
+        if ($_GET['search'])
+        {
+            $products = search_item_by_name($_GET['search']);
+        }
+
+        foreach ($_GET as $key => $val) {
+            if (get_categories()[$key]) {
+                $products = search_item_by_categories($products, array($key => $val));
+            }
+        }
+        $page = isset($_GET['page']) && $_GET['page'] != NULL ? $_GET['page'] : 0;
+
+        if ($products) {
+            $i = $page * 5;
+            foreach ($products as $k => $f) {
+                if ($k >= $i && $k < $i + 5) {
+                    ?>
+                    <div class="item">
+                        <div class="image">
+                            <img src="https://cdn.intra.42.fr/users/<?= $f['name'] ?>.jpg">
+                        </div>
+                        <div class="description">
+                            <div class="name">
+                                <p><?= $f['name'] ?></p>
+                            </div>
+                            <div class="price">Prix : <?= $f['price'] ?>E</div>
+                            <div class="quantity">Annee : <?= $f['categories']['year'] ?></div>
+                            <div class="quantity">Campus : <?= $f['categories']['campus'] ?></div>
+                            <form action="index.php" method="post">
+                                <input class="id" name="id" type="text" value="<?= $f['uid'] ?>">
+                                <input class="submit" name="submit" type="submit" value="Ajouter au panier">
+                            </form>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+            if ($page > 0) {
+                $request = "?page=".($page - 1);
+                if (isset($_GET['page'])) unset($_GET['page']);
+                foreach ($_GET as $key => $value) {
+                    if (is_array($value)) {
+                        foreach ($value as $key1 => $value1) {
+                            $request .= "&" . $key . "[]=" . $value1;
+                        }
+                    } else {
+                        $request .= "&" . $key . "=" . $value;
+                    }
+                }
+                echo "<a href=\"index.php".$request."\">Page precedente</a>";
+            }
+            if (count($products) > $i + 5) {
+                $request = "?page=".($page + 1);
+                if (isset($_GET['page'])) unset($_GET['page']);
+                foreach ($_GET as $key => $value) {
+                    if (is_array($value)) {
+                        foreach ($value as $key1 => $value1) {
+                            $request .= "&" . $key . "[]=" . $value1;
+                        }
+                    } else {
+                        $request .= "&" . $key . "=" . $value;
+                    }
+                }
+                echo "<a href=\"index.php".$request."\">Page suivante</a>";
+            }
+        }
+        ?>
+    </div>
 <?php
 include('assets/templates/footer.php');
 ?>
