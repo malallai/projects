487526ae61208c2a626c1fd2b67224ee4d25baf3
malallai <malallai@student.42.fr>
2019-05-26 17:04:57 +0200

    3 files changed, 47 insertions(+), 38 deletions(-)

--- piscines/php/rle-ru/rush00/index.php
+++ piscines/php/rle-ru/rush00/index.php
@@ -34,7 +34,8 @@ if (is_installed() !== true) {
                         <p><?= $f['name'] ?></p>
                     </div>
                     <div class="price">Prix : <?= $f['price'] ?>E</div>
-                    <div class="quantity">Annee : <?= $f['year'] ?></div>
+                    <div class="quantity">Annee : <?= $f['categories']['year'] ?></div>
+                    <div class="quantity">Campus : <?= $f['categories']['campus'] ?></div>
                     <form action="index.php" method="post">
                         <input class="id" name="id" type="text" value="<?= $f['uid'] ?>">
                         <input class="submit" name="submit" type="submit" value="Ajouter au panier">
