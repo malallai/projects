2fea580c6e5e25cad880574f5d966bbcd182def5
malallai <malallai@student.42.fr>
2019-05-26 16:23:36 +0200

    5 files changed, 104 insertions(+), 46 deletions(-)

--- piscines/php/rle-ru/rush00/change.php
+++ piscines/php/rle-ru/rush00/change.php
@@ -1,6 +1,25 @@
 <?php
 session_start();
 
+else
+{
+    if (isset($_POST['submit'])) {
+        if ($_POST['submit'] === "Add Product") {
+            $new = array();
+            $new['name'] = $_POST['name'];
+            $new['price'] = $_POST['price'];
+            $new['uid'] = rand(0, 100000);
+            $new['year'] = $_POST['year'];
+            create_product($new);
+        } else if ($_POST['submit'] === "Del Product") {
+            delete_product(get_product($_POST['name']));
+        } else if ($_POST['submit'] === "Del User") {
+            delete_user($_POST['name']);
+        }
+        header("Location: admin.php");
+    }
+}
+
 foreach ($file as $k=>$f)
 {
 	if ($f['login'] == $_SESSION['logged_as'])
--- piscines/php/rle-ru/rush00/user.php
+++ piscines/php/rle-ru/rush00/user.php
@@ -5,21 +5,32 @@ if (!isset($_SESSION) || $_SESSION['logged_as'] == '')
 {
     header('location: login.php');
 }
+if (isset($_POST['submit'])) {
+    if ($_POST['submit'] === "Change password") {
+        change_password($_SESSION['logged_as'], $_POST['oldpw'], $_POST['newpw']);
+    } else if ($_POST['submit'] === "Del User") {
+        if (auth($_SESSION['logged_as'], $_POST['pass'])) {
+            $login = $_SESSION['logged_as'];
+            logout();
+            delete_user($login);
+        }
+    }
+}
 if ($_SESSION['logged_as'])
 {
 	echo "<a href='logout.php'>Log out</a><br />";
 	?>
     <a><?= $_SESSION['logged_as'] ?></a>
 	Change password:
-	<form method="POST" action="change.php">
+	<form method="POST" action="user.php">
 		Old password: <input type="password" name="oldpw" />
 		New password: <input type="password" name="newpw" />
 		<input type="submit" name="submit" value="Change password" />
 	</form>
 	Delete account:
-	<form method="POST" action="delete.php">
+	<form method="POST" action="user.php">
 		Password: <input type="password" name="pass" />
-		<input type="submit" name="submit" value="Delete account" />
+		<input type="submit" name="submit" value="Del user" />
 	</form>
 	<?php
 }
