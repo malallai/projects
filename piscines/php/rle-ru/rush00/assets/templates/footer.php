b8301efa556da76206a9270c680d282ade6504c6
malallai <malallai@student.42.fr>
2019-05-26 13:40:13 +0200

    4 files changed, 113 insertions(+), 76 deletions(-)

--- piscines/php/rle-ru/rush00/assets/templates/footer.php
+++ piscines/php/rle-ru/rush00/assets/templates/footer.php
@@ -3,8 +3,9 @@
  * Basically the footer of the front office, seen everywhere.
  */
 ?>
-<div class="flex-footer">
-    <h2>&copy; RLE-RU - MALALLAI</h2>
+    <div class="flex-footer">
+        <h2>&copy; RLE-RU - MALALLAI</h2>
+    </div>
 </div>
 </body>
 </html>
--- piscines/php/rle-ru/rush00/assets/templates/header.php
+++ piscines/php/rle-ru/rush00/assets/templates/header.php
@@ -10,52 +10,53 @@ session_start();
     <title>MiniShop</title>
 </head>
 <body>
-<div class="header">
-    <div class="flex-compononent">
-        <div class="icon">
-            <a href="index.php"><img src="assets/images/42.svg"></a>
-        </div>
-    </div>
-    <div class="flex-compononent">
-        <div class="searchbar">
-            <div class="bar">
-                <input id="searchinput" name="search" placeholder="Search" type="text">
+<div class="container">
+    <div class="header">
+        <div class="flex-compononent">
+            <div class="icon">
+                <a href="index.php"><img src="assets/images/42.svg"></a>
             </div>
-                <a class="search" href="" onclick="this.href='search.php?value='+document.getElementById('searchinput').value">
-                    <div class="icon">
-                        <i class='fas fa-search'></i>
-                    </div>
-                </a>
         </div>
-    </div>
-    <div class="flex-compononent">
-        <div class="icons">
-            <?php
-                if (isset($_SESSION['logged_as']) && $_SESSION['grade'] === 'admin') {
-                    ?>
-                    <div class="flex-icon">
+        <div class="flex-compononent">
+            <div class="searchbar">
+                <div class="bar">
+                    <input id="searchinput" name="search" placeholder="Search" type="text">
+                </div>
+                    <a class="search" href="" onclick="this.href='search.php?value='+document.getElementById('searchinput').value">
                         <div class="icon">
-                            <a href="admin.php"><i class='fas fa-lock'></i></a>
-                        </div>
-                    </div>
-                    <?php
-                }
-            ?>
-            <div class="flex-icon">
-                <div class="icon">
-                    <a href="cart.php">
-                        <i class='fas fa-shopping-cart'></i>
-                        <div class="items-count">
-                            <p><?= isset($_SESSION['items_count']) ? $_SESSION['items_count'] : 0 ?></p>
+                            <i class='fas fa-search'></i>
                         </div>
                     </a>
-                </div>
             </div>
-            <div class="flex-icon">
-                <div class="icon">
-                    <a href="user.php"><i class='fas fa-user'></i></a>
+        </div>
+        <div class="flex-compononent">
+            <div class="icons">
+                <?php
+                    if (isset($_SESSION['logged_as']) && $_SESSION['grade'] === 'admin') {
+                        ?>
+                        <div class="flex-icon">
+                            <div class="icon">
+                                <a href="admin.php"><i class='fas fa-lock'></i></a>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                ?>
+                <div class="flex-icon">
+                    <div class="icon">
+                        <a href="cart.php">
+                            <i class='fas fa-shopping-cart'></i>
+                            <div class="items-count">
+                                <p><?= isset($_SESSION['items_count']) ? $_SESSION['items_count'] : 0 ?></p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex-icon">
+                    <div class="icon">
+                        <a href="user.php"><i class='fas fa-user'></i></a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
