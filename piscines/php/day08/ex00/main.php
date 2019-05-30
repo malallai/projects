388e28dc54fa07b691c1266120aee221a8f4308c
malallai <malallai@student.42.fr>
2019-05-30 19:22:06 +0200

    3 files changed, 37 insertions(+), 27 deletions(-)

--- piscines/php/day08/ex00/main.php
+++ piscines/php/day08/ex00/main.php
@@ -10,28 +10,30 @@
 </head>
 <body>
 <div class="background"></div>
-<div class="container">
-    <div class="top-nav-row">
-        <div class="top-nav-content">
-            <div class="top-nav">
-                <a class="active">New Game</a>
-                <a class="">Scores</a>
+<main>
+    <div class="container">
+        <div class="top-nav-row">
+            <div class="top-nav-content">
+                <div class="top-nav">
+                    <a class="active">New Game</a>
+                    <a class="">Scores</a>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="new-game-row">
-        <div class="new-game-content">
-            <p>New Game</p>
-            <div class="new-game">
-                <form>
-                    <input type="text" id="player1" class="form-control" placeholder="Player 1">
-                    <input type="text" id="player2" class="form-control" placeholder="Player 2">
-                    <input class="btn btn-white" name="play" value="Play" type="submit">
-                </form>
+        <div class="new-game-row">
+            <div class="new-game-content">
+                <p>New Game</p>
+                <div class="new-game">
+                    <form>
+                        <input type="text" id="player1" class="form-control" placeholder="Player 1">
+                        <input type="text" id="player2" class="form-control" placeholder="Player 2">
+                        <input class="btn btn-white" name="play" value="Play" type="submit">
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</main>
 </body>
 </html>
