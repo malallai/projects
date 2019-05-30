b38bab7ad39cc511e1cd9c2b6e6f8a763b786a61
malallai <malallai@student.42.fr>
2019-05-30 14:55:48 +0200

    6 files changed, 208 insertions(+), 172 deletions(-)

--- piscines/php/day09/ex03/delete.php
+++ piscines/php/day09/ex03/delete.php
@@ -1,1 +1,26 @@
 <?php
+if (isset($_POST) && $_POST !== null && $_POST['id'] !== null && $_POST['id'] !== "") {
+    $file = fopen("list.csv", "r");
+    if ($file) {
+        flock($file, LOCK_EX);
+        $array = [];
+        while ($csv = fgetcsv($file, 0, ";")) {
+            $array[] = $csv;
+        }
+        fclose($file);
+        $file = fopen("list.csv", "w");
+        flock($file, LOCK_EX);
+        $i = 0;
+        while ($i < count($array)) {
+            if ($array[$i][0] == $_POST['id']) {
+                break;
+            }
+            $i++;
+        }
+        array_splice($array, $i, 1);
+        foreach ($array as $line) {
+            fputcsv($file, $line, ";");
+        }
+        fclose($file);
+    }
+}
