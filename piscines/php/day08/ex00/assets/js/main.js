995e128a8919fe34e3583f01c4fec487391162f9
malallai <malallai@student.42.fr>
2019-05-30 19:50:29 +0200

    4 files changed, 85 insertions(+), 56 deletions(-)

---
+++ piscines/php/day08/ex00/assets/js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+    var new_game = $("#new-game");
+    var scores = $("#scores");
+
+    new_game.on("click", function () {
+        $('#new-game-form').removeClass('hidden');
+        $('#scores-row').addClass('hidden');
+        new_game.removeClass('active');
+        scores.addClass('active');
+    });
+
+    scores.on("click", function () {
+        $('#new-game-form').addClass('hidden');
+        $('#scores-row').removeClass('hidden');
+        new_game.addClass('active');
+        scores.removeClass('active');
+    });
+})
