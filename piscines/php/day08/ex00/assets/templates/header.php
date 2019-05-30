7cdf53e4474999df48be634814052c2085fb8ba3
malallai <malallai@student.42.fr>
2019-05-30 20:02:51 +0200

    5 files changed, 142 insertions(+), 102 deletions(-)

---
+++ piscines/php/day08/ex00/assets/templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>D08 | malallai</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <script src="assets/js/main.js"></script>
+</head>
+<body>
+<div class="background"></div>
+<main>
+    <div class="container">
