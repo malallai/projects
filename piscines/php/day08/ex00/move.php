032e5a0eb6e93decbb5939938a9a2720620ee1d9
malallai <malallai@student.42.fr>
2019-05-31 17:14:09 +0200

    3 files changed, 19 insertions(+), 22 deletions(-)

--- piscines/php/day08/ex00/move.php
+++ piscines/php/day08/ex00/move.php
@@ -6,8 +6,6 @@ require_once 'classes/Game.class.php';
 require_once 'classes/Player.class.php';
 require_once 'classes/Ship.class.php';
 
-if (isset($_POST['direction'])) {
-    if ($_SESSION['data']) {
         $game = unserialize($_SESSION['data']);
 
         $player = $game->getCurrentPlayer();
@@ -19,5 +17,3 @@ if (isset($_POST['direction'])) {
         echo "pute";
 
         return;
-    }
-}
