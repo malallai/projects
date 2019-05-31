6f3e861b942a12cef2a98af3693faf47593f1e5b
malallai <malallai@student.42.fr>
2019-05-31 17:07:50 +0200

    2 files changed, 56 insertions(+), 39 deletions(-)

--- piscines/php/day08/ex00/assets/js/game.js
+++ piscines/php/day08/ex00/assets/js/game.js
@@ -1,5 +1,7 @@
 $(function () {
     var moves = $(".move-key");
+    getPlayers();
+    getAsteroids();
 
     moves.children().on("click", function () {
         console.log("click " + $(this));
@@ -8,41 +10,56 @@ $(function () {
             console.log(direction);
             $.ajax({
                 url: './move.php',
+                dataType: 'json',
                 type: 'POST',
                 data: {
                     direction: direction
                 }
             }).done(function (data) {
-                console.log(data);
+                if (data[0] === false) {
+
+                } else {
+                    clear();
+                    getPlayers();
+                    getAsteroids();
+                }
             });
         }
     });
 
-    $.ajax({
-        url: './get_players.php',
-        dataType: 'json',
-        type: 'POST',
-        data: {
-            val: "get"
-        }
-    }).done(function (data) {
-        for (var n = 0; n < data.length; n++) {
-            draw_ship(data[n]['ship'], data[n]['color']);
-        }
-    });
+    function clear() {
+        $('td').removeClass('red').removeClass('blue').removeClass('asteroid');
+    }
 
-    $.ajax({
-        url: './get_asteroids.php',
-        dataType: 'json',
-        type: 'POST',
-        data: {
-            val: "get"
-        }
-    }).done(function (data) {
-        for (var n = 0; n < data.length; n++) {
-            draw_asteroid(data[n]['asteroid']);
-        }
-    });
+    function getPlayers() {
+        $.ajax({
+            url: './get_players.php',
+            dataType: 'json',
+            type: 'POST',
+            data: {
+                val: "get"
+            }
+        }).done(function (data) {
+            for (var n = 0; n < data.length; n++) {
+                draw_ship(data[n]['ship'], data[n]['color']);
+            }
+        });
+    }
+
+    function getAsteroids() {
+        $.ajax({
+            url: './get_asteroids.php',
+            dataType: 'json',
+            type: 'POST',
+            data: {
+                val: "get"
+            }
+        }).done(function (data) {
+            for (var n = 0; n < data.length; n++) {
+                draw_asteroid(data[n]['asteroid']);
+            }
+        });
+    }
 
     function draw_ship(data, color) {
         for (var n = 0; n < data.length; n++) {
