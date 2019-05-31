6783257d3ff3a4d93807f72e093fdaf1fe5f2b12
malallai <malallai@student.42.fr>
2019-05-31 16:20:27 +0200

    6 files changed, 175 insertions(+), 65 deletions(-)

--- piscines/php/day08/ex00/assets/js/game.js
+++ piscines/php/day08/ex00/assets/js/game.js
@@ -12,11 +12,32 @@ $(function () {
         }
     });
 
+    $.ajax({
+        url: './get_asteroids.php',
+        dataType: 'json',
+        type: 'POST',
+        data: {
+            val: "get"
+        }
+    }).done(function (data) {
+        for (var n = 0; n < data.length; n++) {
+            draw_asteroid(data[n]['asteroid']);
+        }
+    });
+
     function draw_ship(data, color) {
         for (var n = 0; n < data.length; n++) {
             var x = data[n]['x'];
             var y = data[n]['y'];
             $('tr[id=' + y + '] td[id=' + x + ']').addClass(color);
+        }
+    }
+
+    function draw_asteroid(data) {
+        for (var n = 0; n < data.length; n++) {
+            var x = data[n]['x'];
+            var y = data[n]['y'];
+            $('tr[id=' + y + '] td[id=' + x + ']').addClass('asteroid');
         }
     }
 
--- piscines/php/day08/ex00/classes/Map.class.php
+++ piscines/php/day08/ex00/classes/Map.class.php
@@ -6,6 +6,9 @@ class Map {
     private $_sizeX = 40;
     private $_sizeY = 20;
     private $_grid;
+    private $_asteroid_count = 2;
+    private $_asteroids;
+
 
     public function __construct($sizeX, $sizeY) {
         $this->_sizeX = $sizeX;
@@ -17,6 +20,12 @@ class Map {
             for ($x = 0; $x < $this->_sizeX; $x++) {
                 $this->_grid[$y][$x] = new Void($this, $x, $y);
             }
+        }
+        for ($i = 0; $i < $this->_asteroid_count; $i++) {
+            $x = rand(0, $this->_sizeX);
+            $y = rand(0, $this->_sizeY);
+            $this->_grid[$y][$x] = $tmp = new Asteroid($this, $x, $y);
+            $this->_asteroids[] = $tmp;
         }
     }
 
@@ -38,6 +47,14 @@ class Map {
             echo "</tr>";
         }
         echo "</table>";
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAsteroids()
+    {
+        return $this->_asteroids;
     }
 
 }
