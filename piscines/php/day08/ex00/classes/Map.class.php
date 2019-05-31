b7a43b33f6fb6dbbdf5945abec4ba7be547f8b82
malallai <malallai@student.42.fr>
2019-05-31 15:50:22 +0200

    3 files changed, 67 insertions(+), 77 deletions(-)

--- piscines/php/day08/ex00/classes/Map.class.php
+++ piscines/php/day08/ex00/classes/Map.class.php
@@ -31,9 +31,9 @@ class Map {
     public function draw() {
         echo "<table>";
         for ($y = 0; $y < $this->_sizeY; $y++) {
-            echo "<tr id='y$y'>";
+            echo "<tr id='$y'>";
             for ($x = 0; $x < $this->_sizeX; $x++) {
-                echo "<td id='x$x'></td>";
+                echo "<td id='$x'></td>";
             }
             echo "</tr>";
         }
