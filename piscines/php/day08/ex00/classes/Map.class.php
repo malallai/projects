54fb458fd9af0eb968a7ea8d62bba515ecf62464
malallai <malallai@student.42.fr>
2019-05-31 13:40:12 +0200

    2 files changed, 7 insertions(+), 7 deletions(-)

--- piscines/php/day08/ex00/classes/Map.class.php
+++ piscines/php/day08/ex00/classes/Map.class.php
@@ -11,7 +11,7 @@ class Map {
         for ($y = 0; $y < $this->_sizeY; $y++) {
             echo "<tr id='y=$y'>";
             for ($x = 0; $x < $this->_sizeX; $x++) {
-                echo "<td id='x=$x></td>";
+                echo "<td id='x=$x'></td>";
             }
             echo "</tr>";
         }
