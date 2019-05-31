b2be52326bbd394ef9753322347751424c6db53f
malallai <malallai@student.42.fr>
2019-05-31 17:56:14 +0200

    2 files changed, 37 insertions(+), 42 deletions(-)

--- piscines/php/day08/ex00/get_grid.php
+++ piscines/php/day08/ex00/get_grid.php
@@ -4,13 +4,8 @@ session_start();
 
 require_once 'classes/Game.class.php';
 require_once 'classes/Asteroid.class.php';
-if ($_POST['val'] === "get") {
-    if ($_SESSION['data']) {
         $game = unserialize($_SESSION['data']);
         $array = array();
         $array[] = array("grid" => $game->getMap()->getGrid());
         echo json_encode($array);
         return;
-    }
-    echo json_encode(array("nop"));
-}
