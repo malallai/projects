3296fe05a3490e3b90090bc67585105df3bf99c6
malallai <malallai@student.42.fr>
2019-05-31 13:36:55 +0200

    4 files changed, 41 insertions(+), 65 deletions(-)

--- piscines/php/day08/ex00/index.php
+++ piscines/php/day08/ex00/index.php
@@ -3,6 +3,8 @@
 session_start();
 
 include_once 'assets/templates/header.php';
+require_once 'classes/Map.class.php';
+require_once 'classes/Game.class.php';
 
 if (isset($_SESSION['data'])) {
     $data = unserialize($_SESSION['data']);
