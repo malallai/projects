aeb758fc5c6a88591032253585e507c0b73c3039
malallai <malallai@student.42.fr>
2019-06-16 18:43:00 +0200

    2 files changed, 70 insertions(+), 15 deletions(-)

--- camagru/views/dev/Test.php
+++ camagru/views/dev/Test.php
@@ -1,2 +1,57 @@
-<?php
-echo "test";
+<style>
+    #snackbar {
+        visibility: hidden;
+        min-width: 250px;
+        margin-left: -125px;
+        background-color: #2b2b2b;
+        color: #fff;
+        text-align: center;
+        border-radius: 2px;
+        padding: 16px;
+        position: fixed;
+        z-index: 1;
+        left: 50%;
+        bottom: 30px;
+    }
+
+    #snackbar.show {
+        visibility: visible;
+        -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+        animation: fadein 0.5s, fadeout 0.5s 2.5s;
+    }
+
+    @-webkit-keyframes fadein {
+        from {bottom: 0; opacity: 0;}
+        to {bottom: 30px; opacity: 1;}
+    }
+
+    @keyframes fadein {
+        from {bottom: 0; opacity: 0;}
+        to {bottom: 30px; opacity: 1;}
+    }
+
+    @-webkit-keyframes fadeout {
+        from {bottom: 30px; opacity: 1;}
+        to {bottom: 0; opacity: 0;}
+    }
+
+    @keyframes fadeout {
+        from {bottom: 30px; opacity: 1;}
+        to {bottom: 0; opacity: 0;}
+    }
+</style>
+
+<div id="test">Test</div>
+<div id="flash-error">Error</div>
+
+<script>
+    document.getElementById("test").onclick = async function() {
+        document.getElementById("flash-error").className = "show";
+        await sleep(3000);
+        document.getElementById("flash-error").className = document.getElementById("snackbar").className.replace("show", "");
+    };
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+</script>
