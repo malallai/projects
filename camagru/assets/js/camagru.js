41ab1a6b13c6bae70a80954c07d8e69f493020fb
malallai <malallai@student.42.fr>
2019-06-16 19:14:05 +0200

    2 files changed, 17 insertions(+), 25 deletions(-)

--- camagru/assets/js/camagru.js
+++ camagru/assets/js/camagru.js
@@ -2,7 +2,7 @@ window.onload = function() {
     async function new_snackbar(message) {
         if (document.getElementById("snackbar").className !== "show"
             && document.getElementById("snackbar").className !== "hide") {
-            document.getElementById("inner").innerHTML = document.getElementById("inner").innerHTML + "<div id='snackbar'>" + message + "</div>";
+            document.getElementById("inner").innerHTML = document.getElementById("inner").innerHTML + "<div onclick='hideSnack();' id='snackbar'>" + message + "</div>";
             document.getElementById("snackbar").className = "show";
             await sleep(5000);
             hideSnack();
@@ -14,12 +14,6 @@ window.onload = function() {
         await sleep(500);
         document.getElementById("snackbar").className = document.getElementById("snackbar").className.replace("hide", "");
     }
-
-    document.getElementById("snackbar").onclick = async function() {
-        if (document.getElementById("snackbar").className === "show") {
-            hideSnack();
-        }
-    };
 
     function sleep(ms) {
         if (tmp)
