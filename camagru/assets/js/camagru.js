532319fd2a57923db6a4003a1e46659977a1960b
malallai <malallai@student.42.fr>
2019-06-16 19:14:56 +0200

    2 files changed, 24 insertions(+), 27 deletions(-)

--- camagru/assets/js/camagru.js
+++ camagru/assets/js/camagru.js
@@ -1,24 +1,22 @@
-window.onload = function() {
-    async function new_snackbar(message) {
-        if (document.getElementById("snackbar").className !== "show"
-            && document.getElementById("snackbar").className !== "hide") {
-            document.getElementById("inner").innerHTML = document.getElementById("inner").innerHTML + "<div onclick='hideSnack();' id='snackbar'>" + message + "</div>";
-            document.getElementById("snackbar").className = "show";
-            await sleep(5000);
-            hideSnack();
-        }
+async function new_snackbar(message) {
+    if (document.getElementById("snackbar").className !== "show"
+        && document.getElementById("snackbar").className !== "hide") {
+        document.getElementById("inner").innerHTML = document.getElementById("inner").innerHTML + "<div onclick='hideSnack();' id='snackbar'>" + message + "</div>";
+        document.getElementById("snackbar").className = "show";
+        await sleep(5000);
+        hideSnack();
     }
+}
 
-    async function hideSnack() {
-        document.getElementById("snackbar").className = document.getElementById("snackbar").className.replace("show", "hide");
-        await sleep(500);
-        document.getElementById("snackbar").className = document.getElementById("snackbar").className.replace("hide", "");
-    }
+async function hideSnack() {
+    document.getElementById("snackbar").className = document.getElementById("snackbar").className.replace("show", "hide");
+    await sleep(500);
+    document.getElementById("snackbar").className = document.getElementById("snackbar").className.replace("hide", "");
+}
 
-    function sleep(ms) {
-        if (tmp)
-            clearTimeout(tmp);
-        return new Promise(resolve => (tmp = setTimeout(resolve, ms)));
-    }
-};
+function sleep(ms) {
+    if (tmp)
+        clearTimeout(tmp);
+    return new Promise(resolve => (tmp = setTimeout(resolve, ms)));
+}
 
