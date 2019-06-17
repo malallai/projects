6246bd65480298f08d313ddfb7b61ddfe3c374f8
malallai <malallai@student.42.fr>
2019-06-17 21:42:05 +0200

    6 files changed, 146 insertions(+), 8 deletions(-)

---
+++ camagru/Public/views/user/PwdReset.php
@@ -0,0 +1,21 @@
+<link href="/Public/assets/css/login.css" rel="stylesheet">
+<div class="row">
+    <div class="row-content">
+        <div class="nav-content">
+            <div class="nav">
+                <a id="reset-button" class="active nav-big">Édition du mot de passe</a>
+                <a id="reset-small-button" class="active nav-small"><i class="fas fa-lock"></i></a>
+            </div>
+        </div>
+        <div  id="reset-content" class="active content content-reset">
+            <h2 class="title">Changer de mot de passe</h2>
+            <form method="POST" action="/user/resetpw">
+                <input type="hidden" name="token" value="<?= $token ?>">
+                <input type="hidden" name="reset_token" value="<?= htmlspecialchars(explode('/', $this->_url)[2]) ?>">
+                <input type="password" name="password" id="password" class="form-control" placeholder="Nouveau mot de passe" required="" autofocus="">
+                <input type="password" name="password_repeat" id="password_repeat" class="form-control" placeholder="Répéter" required="">
+                <input class="button black" name="reset" value="Modifier" type="submit">
+            </form>
+        </div>
+    </div>
+</div>
