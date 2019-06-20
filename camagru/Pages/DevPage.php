8da16558b64f1d94ff0e748a816ee00bf10116f8
malallai <malallai@student.42.fr>
2019-06-20 19:51:17 +0200

    57 files changed, 3938 insertions(+), 310 deletions(-)

---
+++ camagru/Pages/DevPage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Pages;
+use Core\Page;
+class DevPage extends Page {
+
+    public function __construct($url) {
+        parent::__construct($url);
+        $this->_template = "templates/general";
+    }
+
+    public function index() {
+        $params = array('content' => 'dev/Test');
+        $this->render($params);
+    }
+
+    public function mail() {
+        $params = array('content' => 'mail/Template');
+        $this->render($params);
+    }
+
+}
