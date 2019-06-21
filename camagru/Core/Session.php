9844568fca3f53d101adb8120e10e945160b796a
malallai <malallai@student.42.fr>
2019-06-21 14:38:33 +0200

    62 files changed, 4243 insertions(+), 310 deletions(-)

--- camagru/Core/Session.php
+++ camagru/Core/Session.php
@@ -4,25 +4,24 @@
 namespace Core;
 class Session {
 
-    public static function checkSession() {
-        return session_status() !== PHP_SESSION_NONE;
+    public static function isStart() {
+        return session_status() == PHP_SESSION_ACTIVE;
     }
 
     public static function startSession() {
-        Snackbar::send_snack(self::checkSession().session_status());
-        if (!self::checkSession()) {
+        if (!self::isStart()) {
             session_start();
         }
     }
 
     public static function destroySession() {
-        if (self::checkSession()) {
+        if (self::isStart()) {
             session_destroy();
         }
     }
 
     public static function resetSession() {
-        if (self::checkSession()) {
+        if (self::isStart()) {
             $_SESSION = array();
             self::destroySession();
         }
