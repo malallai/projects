23e3f2b0de479180aed60a16da107d84d2905ce6
malallai <malallai@student.42.fr>
2019-06-21 11:56:43 +0200

    58 files changed, 4039 insertions(+), 310 deletions(-)

--- camagru/Core/Mail.php
+++ camagru/Core/Mail.php
@@ -1,7 +1,40 @@
 <?php
 
-
 namespace Core;
 class Mail {
+
+    protected $_from;
+    protected $_to;
+    protected $_object;
+    protected $_message;
+
+    public function __construct($to, $from = "camagru@malallai.fr") {
+        $this->_to = $to;
+        $this->_from = $from;
+    }
+
+    /**
+     * @param mixed $object
+     */
+    public function setObject($object)
+    {
+        $this->_object = $object;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message)
+    {
+        $this->_message = $message;
+    }
+
+    public function send() {
+
+    }
+
+    public static function newMail() {
+
+    }
 
 }
