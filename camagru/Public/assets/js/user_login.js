949aeff92944998c426ee1e88da60598dfd6e797
malallai <malallai@student.42.fr>
2019-06-27 12:19:35 +0200

    62 files changed, 4605 insertions(+), 310 deletions(-)

--- camagru/Public/assets/js/user_login.js
+++ camagru/Public/assets/js/user_login.js
@@ -1,4 +1,4 @@
-function init_var() {
+window.onload = function() {
     var login_button = document.getElementById('login-button');
     var register_button = document.getElementById('register-button');
     var reset_button = document.getElementById('reset-button');
@@ -6,36 +6,30 @@ function init_var() {
     var login_content = document.getElementById('login-content');
     var register_content = document.getElementById('register-content');
     var reset_content = document.getElementById('reset-content');
-}
 
-function login_click() {
-    init_var();
-    clear_active();
-    login_button.classList.add("active");
-    login_content.classList.add("active");
-}
+    login_button.addEventListener("click", function() {
+        clear_active();
+        login_button.classList.add("active");
+        login_content.classList.add("active");
+    });
+    register_button.addEventListener("click", function() {
+        clear_active();
+        register_button.classList.add("active");
+        register_content.classList.add("active");
+    });
+    reset_button.addEventListener("click", function() {
+        clear_active();
+        reset_button.classList.add("active");
+        reset_content.classList.add("active");
+    });
 
-function register_click() {
-    init_var();
-    clear_active();
-    register_button.classList.add("active");
-    register_content.classList.add("active");
-}
-
-function reset_click() {
-    init_var();
-    clear_active();
-    reset_button.classList.add("active");
-    reset_content.classList.add("active");
-}
-
-function clear_active() {
-    init_var();
-    login_button.classList.remove("active");
-    register_button.classList.remove("active");
-    reset_button.classList.remove("active");
-    login_content.classList.remove("active");
-    register_content.classList.remove("active");
-    reset_content.classList.remove("active");
-}
+    function clear_active() {
+        login_button.classList.remove("active");
+        register_button.classList.remove("active");
+        reset_button.classList.remove("active");
+        login_content.classList.remove("active");
+        register_content.classList.remove("active");
+        reset_content.classList.remove("active");
+    }
+};
 
