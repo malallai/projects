d02774c11789c823886c9d2b897f7ef1f873f73b
malallai <malallai@student.42.fr>
2019-06-27 11:02:08 +0200

    62 files changed, 4590 insertions(+), 310 deletions(-)

--- camagru/Public/views/templates/general.php
+++ camagru/Public/views/templates/general.php
@@ -25,7 +25,7 @@
             <div class="container">
                 <div class="aside-button button-login">
                     <div class="button-content">
-                        <a href="/user"><span>Connexion</span></a>
+                        <a href="/user">Connexion</a>
                     </div>
                 </div>
             </div>
