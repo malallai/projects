08107ac268f48496f5bf748b66459231a3e443bc
malallai <malallai@student.42.fr>
2019-06-27 15:24:46 +0200

    62 files changed, 4712 insertions(+), 310 deletions(-)

--- camagru/Public/assets/js/camagru.js
+++ camagru/Public/assets/js/camagru.js
@@ -16,7 +16,7 @@ async function new_snackbar(message) {
     child.innerHTML = message;
     div.prepend(child);
     snacks.append(div);
-    document.getElementById("inner").prepend(snacks);
+    document.getElementsByName("main")[0].prepend(snacks);
     await sleep(500, div);
     div.className = div.className.replace("show-anim", "show");
     await sleep(4500, div);
