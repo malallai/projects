75d90064b7cdbcf5c3324e83ac7602abeb06c4a1
malallai <malallai@student.42.fr>
2019-07-02 13:55:23 +0200

    62 files changed, 4576 insertions(+), 310 deletions(-)

--- camagru/Public/views/aside/Profile.php
+++ camagru/Public/views/aside/Profile.php
@@ -5,6 +5,11 @@
             <div class="u-infos">
                 <div class="main-infos">
                     <i class="fas fa-user-alt"></i>
+                    <div class="inline-flex">
+                        <span class="fullname">Malo Allain</span>
+                        <span class="username">malallai</span>
+                    </div>
+                    <div class="usermail">malallai@student.42.fr</div>
                 </div>
 
 
