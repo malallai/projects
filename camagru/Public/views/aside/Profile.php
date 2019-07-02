fddd02503b6fec3f4ad59f66f6f768c9e010fcb7
malallai <malallai@student.42.fr>
2019-07-02 14:43:29 +0200

    63 files changed, 4653 insertions(+), 310 deletions(-)

--- camagru/Public/views/aside/Profile.php
+++ camagru/Public/views/aside/Profile.php
@@ -1,22 +1,20 @@
 <link href="/Public/assets/css/profile.css" rel="stylesheet">
 <div class="user-row">
     <div class="row user-infos-row">
-        <div class="infos">
-            <div class="u-infos">
-                <div class="main-infos">
-                    <div class="icons">
-                        <i class="fas fa-user-alt"></i>
-                    </div>
-                    <div class="main-infos-sub">
-                        <div class="inline-flex">
-                            <span class="fullname">Malo Allain</span>
-                            <span class="username">malallai</span>
-                        </div>
-                        <div class="usermail">malallai@student.42.fr</div>
-                    </div>
+        <div class="u-infos">
+            <div class="main-infos">
+                <div class="icons">
+                    <i class="fas fa-user-alt"></i>
                 </div>
-                <i class="fas fa-cog"></i>
+                <div class="main-infos-sub">
+                    <div class="inline-flex">
+                        <span class="fullname">Malo Allain</span>
+                        <span class="username">malallai</span>
+                    </div>
+                    <div class="usermail">malallai@student.42.fr</div>
+                </div>
             </div>
+            <i class="fas fa-cog"></i>
         </div>
     </div>
 </div>
