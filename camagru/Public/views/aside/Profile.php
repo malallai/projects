8f046a00564cec61c7e6a81eeb2e5f378b16c24d
malallai <malallai@student.42.fr>
2019-07-09 21:34:40 +0200

    63 files changed, 4693 insertions(+), 310 deletions(-)

--- camagru/Public/views/aside/Profile.php
+++ camagru/Public/views/aside/Profile.php
@@ -17,6 +17,32 @@
         </div>
     </a>
 </div>
+<div class="post">
+    <div class="author">
+        <div class="user">
+            <div class="user-pp">
+                <img src="/Public/assets/pictures/users/malo.jpg">
+            </div>
+            <div class="user-infos">
+                <div class="user-name">
+                    <a href="/user/id">Malo</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="picture">
+        <img src="/Public/assets/pictures/posts/3.jpg">
+    </div>
+    <div class="infos">
+        <div class="icons">
+            <a class="link" href="/comment/id"><i class="far fa-comment-alt"></i></a>
+            <a class="link"><i class="far fa-heart"></i></a>
+        </div>
+        <div class="like-counts">
+            <p>10 j'aimes</p>
+        </div>
+    </div>
+</div>
 <div class="aside-button button-logged">
     <div class="manage-pictures button-content">
         <a href="/user/logout"><span>Posts récents</span></a>
