bec1aed34c589581dedaf025a1f7910d2ad1a758
malallai <malallai@student.42.fr>
2019-07-16 16:29:35 +0200

    66 files changed, 5037 insertions(+), 310 deletions(-)

--- camagru/App/Montage/MontageSql.php
+++ camagru/App/Montage/MontageSql.php
@@ -9,6 +9,7 @@ use Exceptions\SqlException;
 class MontageSql extends Sql {
 
     public function upload_picture($user_id, $picture_path) {
+        date_default_timezone_set('UTC');
         try {
             self::init_db();
         } catch (SqlException $e) {
@@ -16,18 +17,9 @@ class MontageSql extends Sql {
             return false;
         }
         try {
-            $result = self::prepare("SELECT password, confirmed FROM users WHERE username = ?", array($username));
-            if (isset($result) && !empty($result)) {
-                if ($result['password'] === $pwd) {
-                    if ($result['confirmed'] == 0) {
-                        Snackbar::send_snack("Please confirm your account before using it.");
-                        return false;
-                    }
-                    return true;
-                }
-            }
-            Snackbar::send_snack("Wrong username or password.");
-            return false;
+            $result = self::prepare("INSERT INTO posts (user_id, image_path, date) VALUES (?,?,?)", array($user_id, $picture_path, date("dd-mm-YYYY HH:ii:ss")));
+            Snackbar::send_snack("Picture send to DB");
+            return true;
         } catch (SqlException $e) {
             Snackbar::send_snack($e->getMessage());
             return false;
--- camagru/Pages/MontagePage.php
+++ camagru/Pages/MontagePage.php
@@ -5,6 +5,7 @@ namespace Pages;
 
 use Core\Page;
 use App\Montage\MontageController;
+use Core\Snackbar;
 
 class MontagePage extends Page  {
 
@@ -17,6 +18,20 @@ class MontagePage extends Page  {
     public function index() {
         $params = array('content' => 'general/Montage');
         $this->render($params);
+    }
+
+    public function new_post($picture) {
+        if ($this->_controller->getUserController()->isLogged()) {
+            if ($this->_controller->getSql()->upload_picture($picture, $this->_controller->getUserController()->get_user()['userid'])) {
+                Snackbar::send_snack("Post uploded");
+            } else {
+                Snackbar::send_snack("Error while uploading post");
+            }
+            $this->redirect("/montage");
+        } else {
+            Snackbar::send_snack("Please log-in");
+            $this->redirect("/");
+        }
     }
 
 }
