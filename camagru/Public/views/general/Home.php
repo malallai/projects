d833b6fdee86b2719ed32c26453c53bb4edd0ed5
malallai <malallai@student.42.fr>
2019-07-17 18:55:06 +0200

    3 files changed, 18 insertions(+), 20 deletions(-)

--- camagru/Public/views/general/Home.php
+++ camagru/Public/views/general/Home.php
@@ -40,15 +40,11 @@
 
         <div class="pagination">
             <div class="inputs">
-                <a class="input arrow" href="/page/1">
-                    <i class="fas fa-angle-left"></i>
-                </a>
+                <a class="input arrow" href="/page/1"><i class="fas fa-angle-left"></i></a>
                 <a class="input page active" href="/page/1">1</a>
                 <a class="input page" href="/page/2">2</a>
                 <a class="input page" href="/page/3">3</a>
-                <a class="input arrow" href="/page/3">
-                    <i class="fas fa-angle-right"></i>
-                </a>
+                <a class="input arrow" href="/page/3"><i class="fas fa-angle-right"></i></a>
             </div>
         </div>
     </div>
