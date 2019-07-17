265702574af2746701205e0c6adf648df0f7e9c3
malallai <malallai@student.42.fr>
2019-07-17 20:04:42 +0200

    2 files changed, 8 insertions(+), 8 deletions(-)

--- camagru/Public/views/aside/Profile.php
+++ camagru/Public/views/aside/Profile.php
@@ -9,9 +9,9 @@
                 <div class="main-infos-sub">
                     <div class="inline-flex">
                         <span class="fullname"><?= $params['details']['user']['first_name']." ".$params['details']['user']['last_name'] ?></span>
-                        <span class="username">malallai</span>
+                        <span class="username"><?= $params['details']['user']['username']?></span>
                     </div>
-                    <div class="usermail">malallai@student.42.fr</div>
+                    <div class="usermail"><?= $params['details']['user']['email']?></div>
                 </div>
             </div>
         </div>
