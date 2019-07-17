8f30e0980b6d2f22aa22e1c72b44671d517276fa
malallai <malallai@student.42.fr>
2019-07-17 17:09:24 +0200

    2 files changed, 33 insertions(+), 37 deletions(-)

--- camagru/App/Montage/MontageSql.php
+++ camagru/App/Montage/MontageSql.php
@@ -10,10 +10,7 @@ class MontageSql extends Sql {
 
     public function upload_picture($user_id, $picture_path) {
         try {
-            $request = self::run("INSERT INTO posts (user_id, image_path) VALUES (?,?)", array($user_id, $picture_path));
-            $result = $request[0];
-            var_dump($result['result']);
-            Snackbar::send_snack($user_id." ".$picture_path);
+            self::run("INSERT INTO posts (user_id, image_path) VALUES (?,?)", array($user_id, $picture_path));
             return true;
         } catch (SqlException $e) {
             Snackbar::send_snack($e->getMessage());
