b42b54bf7e38b39641139a50ca25bcf0adb43b9f
malallai <malallai@student.42.fr>
2019-07-17 20:02:56 +0200

    4 files changed, 56 insertions(+), 42 deletions(-)

--- camagru/App/User/UserSql.php
+++ camagru/App/User/UserSql.php
@@ -169,7 +169,7 @@ class UserSql extends Sql {
         try {
             $posts = self::runList("SELECT posts.*, (SELECT COUNT(*) FROM likes WHERE likes.post_id = posts.id) AS likes FROM posts INNER JOIN users ON posts.user_id = users.id WHERE users.id = ? ORDER BY date DESC ",  array($id), PDO::FETCH_ASSOC);
             $user = self::run("SELECT users.username, users.first_name, users.last_name, users.email FROM users WHERE id = ?", array($id));
-            return array("posts" => $posts['result'], "user" => $user['result']);
+            return array('posts' => $posts['result'], 'user' => $user['result']);
         } catch (SqlException $e) {
             Snackbar::send_snack($e->getMessage());
             return false;
