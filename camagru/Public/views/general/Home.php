2c5c5275bb0ecd36ccfe4c10c6a1afa22d9b2214
malallai <malallai@student.42.fr>
2019-07-17 19:30:41 +0200

    2 files changed, 5 insertions(+), 5 deletions(-)

--- camagru/Public/views/general/Home.php
+++ camagru/Public/views/general/Home.php
@@ -44,7 +44,7 @@
                 <a class="input arrow <?= $page == 1 ? "disable" : "" ?>" href="/page/<?= $page - 1?>"><i class="fas fa-angle-left"></i></a>
                 <?php
                     $s = ($page == 1 || $page == $pages ? ($page == $pages ? $page - 2 : $page) : $page - 1);
-                    for ($i = $s; $i < ($page == $pages ? $page : $s + 3); $i++) {
+                    for ($i = $s; $i < $s + 3; $i++) {
                     ?>
                         <a class="input page <?= $i == $page ? "active" : "" ?>" href="/page/<?=$i?>"><?=$i?></a>
                     <?php
