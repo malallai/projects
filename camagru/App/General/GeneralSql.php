f19261eaf12ca37f9d87698636440949acbae27e
malallai <malallai@student.42.fr>
2019-07-17 16:11:26 +0200

    2 files changed, 9 insertions(+), 9 deletions(-)

--- camagru/App/General/GeneralSql.php
+++ camagru/App/General/GeneralSql.php
@@ -24,7 +24,7 @@ class GeneralSql extends Sql {
             Snackbar::send_snack("Posts : ".$postsCount);
             Snackbar::send_snack("Start : ".$start);
             Snackbar::send_snack("End : ".$end);
-            $request = self::runList("SELECT * FROM posts DESC WHERE id BETWEEN ? AND ? ORDER BY id DESC",  array($start, $end), PDO::FETCH_ASSOC);
+            $request = self::runList("SELECT * FROM posts DESC WHERE id BETWEEN ? AND ?",  array($start, $end), PDO::FETCH_ASSOC);
             return $request['result'];
         } catch (SqlException $e) {
             Snackbar::send_snack($e->getMessage());
