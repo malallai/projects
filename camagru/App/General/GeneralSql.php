1d5ab1775cef13b5296b00d04d582d61f35ff866
malallai <malallai@student.42.fr>
2019-07-17 14:13:32 +0200

    2 files changed, 19 insertions(+), 19 deletions(-)

--- camagru/App/General/GeneralSql.php
+++ camagru/App/General/GeneralSql.php
@@ -19,7 +19,7 @@ class GeneralSql extends Sql {
             if(!($page > 0 AND $page <= $tot))
                 return false;
             $start = ($page - 1) * $postsPerPage;
-            $request = self::run("SELECT * FROM posts WHERE id BETWEEN ? AND ?",  array(0, 5));
+            $request = self::run("SELECT * FROM posts",  array(0, 5));
             return $request;
         } catch (SqlException $e) {
             Snackbar::send_snack($e->getMessage());
