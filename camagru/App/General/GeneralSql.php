3b6f25b7530d43b5984693c3a3f2b1fa487a5c98
malallai <malallai@student.42.fr>
2019-07-17 12:37:04 +0200

    3 files changed, 77 insertions(+), 40 deletions(-)

--- camagru/App/General/GeneralSql.php
+++ camagru/App/General/GeneralSql.php
@@ -2,8 +2,34 @@
 
 
 namespace App\General;
+use Core\Snackbar;
 use Core\Sql;
+use Exceptions\SqlException;
 
 class GeneralSql extends Sql {
+
+    public function getPosts($page = 1) {
+        try {
+            self::init_db();
+        } catch (SqlException $e) {
+            Snackbar::send_snack($e->getMessage());
+            return false;
+        }
+        try {
+            $postsPerPage = 5;
+            $posts = $this->_controller->getSql()->prepare("SELECT id FROM posts");
+            $postsCount = $posts->rowCount();
+            $tot = ceil($postsCount / $postsPerPage);
+            if(!($page > 0 AND $page <= $tot))
+                return false;
+            $start = ($page - 1) * $postsPerPage;
+            //$this->_controller->getSql()->prepare("SELECT posts.*, users.username, (SELECT COUNT(*) FROM likes WHERE likes.post_id = posts.id) AS likes FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.id = ?");
+            $result = $this->_controller->getSql()->prepare("SELECT * FROM posts ORDER BY id DESC LIMIT ?,?", array($start, $postsPerPage));
+            var_dump($result);
+        } catch (SqlException $e) {
+            Snackbar::send_snack($e->getMessage());
+            return false;
+        }
+    }
 
 }
