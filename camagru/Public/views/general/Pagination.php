3b32868d92617a2ec69c0942cd69edf12529f9ba
malallai <malallai@student.42.fr>
2019-07-19 15:08:34 +0200

    72 files changed, 5098 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Pagination.php
+++ camagru/Public/views/general/Pagination.php
@@ -7,7 +7,8 @@ $page = $params['page'];
         <a class="input arrow <?=$page == 1 ? "disable" : "" ?>" <?=$page == 1 ? "" : "href='/page/" . ($page - 1) . "'" ?>><i class="fas fa-angle-left"></i></a>
         <?php
         $s = ($page == 1 || $page == $pages ? ($page == $pages ? $page - 2 : $page) : $page - 1);
-        for ($i = $s;$i < $s + 3;$i++) {
+        $x = $page == $pages ? 1 : ($pages < 3 ? $pages : 3);
+        for ($i = $s; $i < $s + $x; $i++) {
             ?>
             <a class="input page <?=$i == $page ? "active" : "" ?>" href="/page/<?=$i ?>"><?=$i ?></a>
             <?php
