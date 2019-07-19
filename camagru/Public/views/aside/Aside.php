baba8cc4c7d15124d6d25e865bb88f29f4b80255
malallai <malallai@student.42.fr>
2019-07-19 17:54:44 +0200

    76 files changed, 5205 insertions(+), 310 deletions(-)

---
+++ camagru/Public/views/aside/Aside.php
@@ -0,0 +1,13 @@
+<div class="aside-bar">
+    <div class="head">
+        <div class="title">Mon Compte</div>
+        <div class="icon">
+            <a href="" onclick="overlay()">
+                <i class='fas fa-times'></i>
+            </a>
+        </div>
+    </div>
+    <div class="aside-content">
+        <object type="text/html" data="/Public/views/user/AsideProfile.php"></object>
+    </div>
+</div>
