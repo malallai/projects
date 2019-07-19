7f594d5feb1f1622cf2b2a760f9c99554ea953e3
malallai <malallai@student.42.fr>
2019-07-19 14:28:35 +0200

    71 files changed, 5180 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Pagination.php
+++ camagru/Public/views/general/Pagination.php
@@ -1,6 +1,6 @@
 <div class="pagination">
     <div class="inputs">
-        <?php $page = $params['page']; $pages = $params['pages'] ?>
+        <?php $pages = $params['pages']; $page = $pages == 0 ? 1 : $params['page'] ?>
         <a class="input arrow <?= $page == 1 ? "disable" : "" ?>" <?= $page == 1 ? "" : "href='/page/".($page - 1)."'"?>><i class="fas fa-angle-left"></i></a>
         <?php
         $s = ($page == 1 || $page == $pages ? ($page == $pages ? $page - 2 : $page) : $page - 1);
@@ -10,6 +10,6 @@
             <?php
         }
         ?>
-        <a class="input arrow <?= $page == $params['pages'] ? "disable" : "" ?>" <?= $page == $params['pages'] ? "" : "href='/page/".($page + 1)."'"?>><i class="fas fa-angle-right"></i></a>
+        <a class="input arrow <?= $page == $pages ? "disable" : "" ?>" <?= $page == $pages ? "" : "href='/page/".($page + 1)."'"?>><i class="fas fa-angle-right"></i></a>
     </div>
 </div>
