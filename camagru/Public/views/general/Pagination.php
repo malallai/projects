585d3d408b02b413f2b4b2daf5598afd32103e8b
malallai <malallai@student.42.fr>
2019-07-19 15:17:43 +0200

    72 files changed, 5098 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Pagination.php
+++ camagru/Public/views/general/Pagination.php
@@ -6,9 +6,8 @@ $page = $params['page'];
     <div class="inputs">
         <a class="input arrow <?=$page == 1 ? "disable" : "" ?>" <?=$page == 1 ? "" : "href='/page/" . ($page - 1) . "'" ?>><i class="fas fa-angle-left"></i></a>
         <?php
-        \Core\Snackbar::send_snack($pages);
         $s = $pages > 2 ? ($page == 1 ? 1 : ($page == $pages ? $page - 2 : $page - 1)) : ($pages == 1 ? 1 : ($page == $pages ? $page - 1 : 1));
-        $x = $pages > 2 ? 3 : ($pages == 1 ? 0 : 1);
+        $x = $pages > 2 ? 3 : ($pages == 1 ? 1 : 2);
         for ($i = $s; $i < $s + $x; $i++) {
             ?>
             <a class="input page <?=$i == $page ? "active" : "" ?>" href="/page/<?=$i ?>"><?=$i ?></a>
