b006acbd1852eac7f823dcaecb76b57c443abe4b
malallai <malallai@student.42.fr>
2019-07-19 14:41:31 +0200

    71 files changed, 5197 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Home.php
+++ camagru/Public/views/general/Home.php
@@ -1,44 +1,52 @@
 <link href="/Public/assets/css/home.css" rel="stylesheet">
 <div class="home-row">
     <div id="posts-row" class="row posts-row">
-        <?= $this->pagination($params); ?>
+        <?=$this->pagination($params); ?>
         <div class="posts">
             <?php
+            if ($pages > 0) {
                 foreach ($params['posts'] as $post) {
                     $details = $this->getPostDetails($post['id']);
                     $userName = $details['username'];
                     $likes = $details['likes'];
-                    $imagePath = $details['image_path'];
-            ?>
-            <div class="post">
-                <div class="author">
-                    <div class="user">
-                        <div class="user-pp">
-                            <img src="/Public/assets/pictures/users/<?= $userName ?>.jpg">
+                    $imagePath = $details['image_path']; ?>
+                    <div class="post">
+                        <div class="author">
+                            <div class="user">
+                                <div class="user-pp">
+                                    <img src="/Public/assets/pictures/users/<?= $userName
+                                    ?>.jpg">
+                                </div>
+                                <div class="user-infos">
+                                    <div class="user-name">
+                                        <a href="/user/id"><?= $userName
+                                            ?> | <?= $post['id'] ?></a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="user-infos">
-                            <div class="user-name">
-                                <a href="/user/id"><?= $userName ?> | <?= $post['id'] ?></a>
+                        <div class="picture">
+                            <img src="<?= $imagePath ?>">
+                        </div>
+                        <div class="infos">
+                            <div class="icons">
+                                <a class="link" href="/comment/id"><i class="far fa-comment-alt"></i></a>
+                                <a class="link"><i class="far fa-heart"></i></a>
+                            </div>
+                            <div class="like-counts">
+                                <p><?= $likes ?><?= $likes > 1 ? " j'aimes" : " j'aime" ?></p>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="picture">
-                    <img src="<?= $imagePath ?>">
-                </div>
-                <div class="infos">
-                    <div class="icons">
-                        <a class="link" href="/comment/id"><i class="far fa-comment-alt"></i></a>
-                        <a class="link"><i class="far fa-heart"></i></a>
-                    </div>
-                    <div class="like-counts">
-                        <p><?= $likes ?><?= $likes > 1 ? " j'aimes" : " j'aime" ?></p>
-                    </div>
-                </div>
-            </div>
-            <?php } ?>
+                    <?php
+                }
+            } else {
+                ?>
+                <h1>Il n'y a aucun poste pour le moment.</h1>
+                <?php
+            } ?>
         </div>
-        <?= $this->pagination($params); ?>
+        <?=$this->pagination($params); ?>
     </div>
     <div id="users-row" class="row users-row">
         <div class="content">
@@ -50,22 +58,25 @@
                 foreach ($params['users'] as $user) {
                     $details = $this->getUserDetails($user['id']);
                     $userName = $details['username'];
-                    $posts = $details['posts'];
-                ?>
-                <div class="user">
-                    <a href="/user" class="user-pp">
-                        <img src="/Public/assets/pictures/users/<?= $userName ?>.jpg">
-                    </a>
-                    <div class="user-infos">
-                        <div class="user-name">
-                            <a href="/user/id"><?= $userName ?></a>
-                        </div>
-                        <div class="posts-count">
-                            <p><?= $posts ?><?= $posts > 1 ? " photos" : " photo" ?></p>
+                    $posts = $details['posts']; ?>
+                    <div class="user">
+                        <a href="/user" class="user-pp">
+                            <img src="/Public/assets/pictures/users/<?=$userName
+                            ?>.jpg">
+                        </a>
+                        <div class="user-infos">
+                            <div class="user-name">
+                                <a href="/user/id"><?=$userName
+                                    ?></a>
+                            </div>
+                            <div class="posts-count">
+                                <p><?=$posts
+                                    ?><?=$posts > 1 ? " photos" : " photo" ?></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <?php } ?>
+                    <?php
+                } ?>
             </div>
         </div>
     </div>
