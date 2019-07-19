037fb43483e45b401027b9e3ccfd9857a59b455e
malallai <malallai@student.42.fr>
2019-07-19 17:54:44 +0200

    76 files changed, 5205 insertions(+), 310 deletions(-)

--- camagru/Public/views/templates/general.php
+++ camagru/Public/views/templates/general.php
@@ -13,17 +13,7 @@
 </head>
 <body>
     <div class="aside-row">
-        <div class="aside-bar">
-            <div class="head">
-                <div class="title">Mon Compte</div>
-                <div class="icon">
-                    <a href="" onclick="overlay()">
-                        <i class='fas fa-times'></i>
-                    </a>
-                </div>
-            </div>
-            <div class="aside-content"></div>
-        </div>
+
     </div>
     <div class="overlay" onclick="overlay()"></div>
     <div class="container">
