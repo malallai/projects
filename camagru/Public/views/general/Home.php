7aa6a8616bc29d405963f0c2037ecaf1438d71b1
malallai <malallai@student.42.fr>
2019-07-19 14:53:37 +0200

    72 files changed, 5192 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Home.php
+++ camagru/Public/views/general/Home.php
@@ -1,55 +1,47 @@
 <link href="/Public/assets/css/home.css" rel="stylesheet">
 <div class="home-row">
-    <?php if ($params['pages'] > 0) { ?>
-        <div id="posts-row" class="row posts-row">
-            <?=$this->pagination($params); ?>
-            <div class="posts">
-                <?php
-                foreach ($params['posts'] as $post) {
-                    $details = $this->getPostDetails($post['id']);
-                    $userName = $details['username'];
-                    $likes = $details['likes'];
-                    $imagePath = $details['image_path']; ?>
-                    <div class="post">
-                        <div class="author">
-                            <div class="user">
-                                <div class="user-pp">
-                                    <img src="/Public/assets/pictures/users/<?=$userName
-                                    ?>.jpg">
-                                </div>
-                                <div class="user-infos">
-                                    <div class="user-name">
-                                        <a href="/user/id"><?=$userName
-                                            ?> | <?=$post['id'] ?></a>
-                                    </div>
-                                </div>
+    <div id="posts-row" class="row posts-row">
+        <?=$this->pagination($params); ?>
+        <div class="posts">
+            <?php
+            foreach ($params['posts'] as $post) {
+                $details = $this->getPostDetails($post['id']);
+                $userName = $details['username'];
+                $likes = $details['likes'];
+                $imagePath = $details['image_path']; ?>
+                <div class="post">
+                    <div class="author">
+                        <div class="user">
+                            <div class="user-pp">
+                                <img src="/Public/assets/pictures/users/<?=$userName
+                                ?>.jpg">
                             </div>
-                        </div>
-                        <div class="picture">
-                            <img src="<?=$imagePath ?>">
-                        </div>
-                        <div class="infos">
-                            <div class="icons">
-                                <a class="link" href="/comment/id"><i class="far fa-comment-alt"></i></a>
-                                <a class="link"><i class="far fa-heart"></i></a>
-                            </div>
-                            <div class="like-counts">
-                                <p><?=$likes ?><?=$likes > 1 ? " j'aimes" : " j'aime" ?></p>
+                            <div class="user-infos">
+                                <div class="user-name">
+                                    <a href="/user/id"><?=$userName
+                                        ?> | <?=$post['id'] ?></a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <?php
-                } ?>
-            </div>
-            <?=$this->pagination($params); ?>
+                    <div class="picture">
+                        <img src="<?=$imagePath ?>">
+                    </div>
+                    <div class="infos">
+                        <div class="icons">
+                            <a class="link" href="/comment/id"><i class="far fa-comment-alt"></i></a>
+                            <a class="link"><i class="far fa-heart"></i></a>
+                        </div>
+                        <div class="like-counts">
+                            <p><?=$likes ?><?=$likes > 1 ? " j'aimes" : " j'aime" ?></p>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            } ?>
         </div>
-        <?php
-    } else {
-        ?>
-        <h2 style="margin: auto;">Il n'y a aucun poste pour le moment.</h2>
-        <?php
-    }
-    ?>
+        <?=$this->pagination($params); ?>
+    </div>
     <div id="users-row" class="row users-row">
         <div class="content">
             <div class="row-title">
