4be35c0fbb842f885b614ffca761eac42f3c2849
malallai <malallai@student.42.fr>
2019-07-19 18:02:35 +0200

    76 files changed, 5195 insertions(+), 310 deletions(-)

--- camagru/Public/views/templates/general.php
+++ camagru/Public/views/templates/general.php
@@ -13,7 +13,19 @@
 </head>
 <body>
     <div class="aside-row">
-
+        <div class="aside-bar">
+            <div class="head">
+                <div class="title">Mon Compte</div>
+                <div class="icon">
+                    <a href="" onclick="overlay()">
+                        <i class='fas fa-times'></i>
+                    </a>
+                </div>
+            </div>
+            <div class="aside-content">
+                <object type="text/html" data="/Public/views/user/AsideProfile.php"></object>
+            </div>
+        </div>
     </div>
     <div class="overlay" onclick="overlay()"></div>
     <div class="container">
