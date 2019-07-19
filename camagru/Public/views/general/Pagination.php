11a623f73d6d48bf8d28fce1a0d2a9cc6a8286d7
malallai <malallai@student.42.fr>
2019-07-19 14:53:37 +0200

    72 files changed, 5192 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Pagination.php
+++ camagru/Public/views/general/Pagination.php
@@ -1,20 +1,18 @@
 <?php
 $pages = $params['pages'];
 $page = $params['page'];
-if ($pages > 0) { ?>
-    <div class="pagination">
-        <div class="inputs">
-            <a class="input arrow <?=$page == 1 ? "disable" : "" ?>" <?=$page == 1 ? "" : "href='/page/" . ($page - 1) . "'" ?>><i class="fas fa-angle-left"></i></a>
-            <?php
-            $s = ($page == 1 || $page == $pages ? ($page == $pages ? $page - 2 : $page) : $page - 1);
-            for ($i = $s;$i < $s + 3;$i++) {
-                ?>
-                <a class="input page <?=$i == $page ? "active" : "" ?>" href="/page/<?=$i ?>"><?=$i ?></a>
-                <?php
-            }
+?>
+<div class="pagination">
+    <div class="inputs">
+        <a class="input arrow <?=$page == 1 ? "disable" : "" ?>" <?=$page == 1 ? "" : "href='/page/" . ($page - 1) . "'" ?>><i class="fas fa-angle-left"></i></a>
+        <?php
+        $s = ($page == 1 || $page == $pages ? ($page == $pages ? $page - 2 : $page) : $page - 1);
+        for ($i = $s;$i < $s + 3;$i++) {
             ?>
-            <a class="input arrow <?=$page == $pages ? "disable" : "" ?>" <?=$page == $pages ? "" : "href='/page/" . ($page + 1) . "'" ?>><i class="fas fa-angle-right"></i></a>
-        </div>
+            <a class="input page <?=$i == $page ? "active" : "" ?>" href="/page/<?=$i ?>"><?=$i ?></a>
+            <?php
+        }
+        ?>
+        <a class="input arrow <?=$page == $pages ? "disable" : "" ?>" <?=$page == $pages ? "" : "href='/page/" . ($page + 1) . "'" ?>><i class="fas fa-angle-right"></i></a>
     </div>
-    <?php
-} ?>
+</div>
