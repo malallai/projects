72e4615989b00f1bc4e812b820e831631400e47b
malallai <malallai@student.42.fr>
2019-07-22 16:22:36 +0200

    74 files changed, 5280 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Home.php
+++ camagru/Public/views/general/Home.php
@@ -27,13 +27,15 @@
                             <img src="<?=$imagePath ?>">
                         </span>
                         <div class="comments">
-                            <div class="comment">
-                                <div class="content">
-                                    <div class="comment-author">
-                                        <a>malallai</a>
-                                    </div>
-                                    <div class="comment-message">
-                                        Blblbl
+                            <div class="row">
+                                <div class="comment">
+                                    <div class="content">
+                                        <div class="comment-author">
+                                            <a>malallai</a>
+                                        </div>
+                                        <div class="comment-message">
+                                            Blblbl
+                                        </div>
                                     </div>
                                 </div>
                             </div>
