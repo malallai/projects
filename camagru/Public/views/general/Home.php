96a37a9d81b5b93657ffa046331934e45c238bb7
malallai <malallai@student.42.fr>
2019-07-22 16:21:34 +0200

    74 files changed, 5278 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Home.php
+++ camagru/Public/views/general/Home.php
@@ -27,37 +27,13 @@
                             <img src="<?=$imagePath ?>">
                         </span>
                         <div class="comments">
-                            <div class="row">
+                            <div class="comment">
                                 <div class="content">
-                                    <div class="comment">
-                                        <div class="content">
-                                            <div class="comment-author">
-                                                <a>malallai</a>
-                                            </div>
-                                            <div class="comment-message">
-                                                Blblbl
-                                            </div>
-                                        </div>
+                                    <div class="comment-author">
+                                        <a>malallai</a>
                                     </div>
-                                    <div class="comment">
-                                        <div class="content">
-                                            <div class="comment-author">
-                                                <a>malallai</a>
-                                            </div>
-                                            <div class="comment-message">
-                                                Blblbl
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="comment">
-                                        <div class="content">
-                                            <div class="comment-author">
-                                                <a>malallai</a>
-                                            </div>
-                                            <div class="comment-message">
-                                                Blblbl
-                                            </div>
-                                        </div>
+                                    <div class="comment-message">
+                                        Blblbl
                                     </div>
                                 </div>
                             </div>
