1bd817250f8b9e6575c4f7b8fb53473bac10361b
malallai <malallai@student.42.fr>
2019-07-29 15:45:43 +0200

    78 files changed, 5556 insertions(+), 310 deletions(-)

--- camagru/Public/assets/js/posts.js
+++ camagru/Public/assets/js/posts.js
@@ -3,9 +3,6 @@ function like() {
     $.ajax({
         url: '/post/' + post.id + '/like',
         type: 'POST',
-        data: {
-            type: type
-        },
         success: function(msg) {
             console.log(msg);
         }
