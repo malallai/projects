0ec2100e1ece231c604075cfbbeeff83c150cfa8
malallai <malallai@student.42.fr>
2019-07-29 17:04:36 +0200

    78 files changed, 5598 insertions(+), 310 deletions(-)

--- camagru/App/Post/PostSql.php
+++ camagru/App/Post/PostSql.php
@@ -18,15 +18,25 @@ class PostSql extends Sql {
         }
     }
 
+    public function getLikeCount($post) {
+        try {
+            $result = self::run("SELECT COUNT(*) FROM likes WHERE post_id = ?", array($post));
+            return ($result[0]);
+        } catch (SqlException $e) {
+            Snackbar::sendSnack($e->getMessage());
+            return false;
+        }
+    }
+
     public function like($id, $user) {
         try {
             $result = self::run("SELECT COUNT(*) FROM likes WHERE post_id = ? AND user_id = ?", array($id, $user));
             if ($result['result'][0] == 1) {
                 self::run("DELETE FROM likes WHERE post_id = ? AND user_id = ?", array($id, $user));
-                return "unlike";
+                return "unlike/".$this->getLikeCount($id);
             } else {
                 self::run("INSERT INTO likes (post_id, user_id) VALUES(?, ?)", array($id, $user));
-                return "like";
+                return "like/".$this->getLikeCount($id);
             }
         } catch (SqlException $e) {
             Snackbar::sendSnack($e->getMessage());
