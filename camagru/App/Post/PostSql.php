1a91dc693c4176fc9233efb9bc1bcf8d5d26056a
malallai <malallai@student.42.fr>
2019-07-29 21:25:45 +0200

    78 files changed, 5667 insertions(+), 310 deletions(-)

--- camagru/App/Post/PostSql.php
+++ camagru/App/Post/PostSql.php
@@ -33,11 +33,21 @@ class PostSql extends Sql {
             $result = self::run("SELECT COUNT(*) FROM likes WHERE post_id = ? AND user_id = ?", array($id, $user));
             if ($result['result'][0] == 1) {
                 self::run("DELETE FROM likes WHERE post_id = ? AND user_id = ?", array($id, $user));
-                return "unlike/".$this->getLikeCount($id);
+                return array("status" => "unlike", "likes" => $this->getLikeCount($id));
             } else {
                 self::run("INSERT INTO likes (post_id, user_id) VALUES(?, ?)", array($id, $user));
-                return "like/".$this->getLikeCount($id);
+                return array("status" => "like", "likes" => $this->getLikeCount($id));
             }
+        } catch (SqlException $e) {
+            Snackbar::sendSnack($e->getMessage());
+            return false;
+        }
+    }
+
+    public function newComment($post, $message, $user) {
+        try {
+            $result = self::run("INSERT INTO comments (post_id, user_id, comment) VALUES(?,?,?)", array($post, $user['id'], $message));
+            return array("status" => "ok", "message" => $message, "author" => $user['username']);
         } catch (SqlException $e) {
             Snackbar::sendSnack($e->getMessage());
             return false;
@@ -47,7 +57,7 @@ class PostSql extends Sql {
     public function delete($post) {
         try {
             $result = self::run("DELETE FROM posts WHERE id = ?", array($post));
-            return "deleted";
+            return array("status" => "deleted");
         } catch (SqlException $exception) {
             Snackbar::sendSnack($exception->getMessage());
         }
