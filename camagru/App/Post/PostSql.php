81ab8051b16b2d5eabebcb1fc6c3975f0dac2477
malallai <malallai@student.42.fr>
2019-07-29 16:25:16 +0200

    78 files changed, 5511 insertions(+), 310 deletions(-)

--- camagru/App/Post/PostSql.php
+++ camagru/App/Post/PostSql.php
@@ -12,7 +12,7 @@ class PostSql extends Sql {
         try {
             $result = self::run("SELECT COUNT(*) FROM posts WHERE id = ?", array($id));
             var_dump($result);
-            echo "\n\n\n";
+            printf("\n\n\n");
             return ($result['result'][0][0] == 1 ? true : false);
         } catch (SqlException $e) {
             Snackbar::sendSnack($e->getMessage());
@@ -24,7 +24,7 @@ class PostSql extends Sql {
         try {
             $result = self::run("SELECT COUNT(*) FROM likes WHERE user_id = ? AND post_id = ?", array($user, $id));
             var_dump($result);
-            echo "\n\n\n";
+            printf("\n\n\n");
             if ($result['result'][0][0] == 1) {
                 self::run("DELETE FROM likes WHERE post_id = ? AND user_id = ?", array($id, $user));
             } else {
