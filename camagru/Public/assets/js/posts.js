17d8ae2943416e8d95f11a5d0a4fbc7af2b43501
malallai <malallai@student.42.fr>
2019-07-29 15:49:35 +0200

    78 files changed, 5561 insertions(+), 310 deletions(-)

--- camagru/Public/assets/js/posts.js
+++ camagru/Public/assets/js/posts.js
@@ -1,5 +1,6 @@
 function like() {
-    let post = this;
+    event.preventDefault();
+    let post = event.srcElement;
     let url = '/post/' + post.id + '/like';
     console.log(url);
     $.ajax({
@@ -9,4 +10,26 @@ function like() {
             console.log(msg);
         }
     });
+}
+
+function focusCommentInput() {
+    event.preventDefault();
+    let body = document.getElementsByTagName("body")[0];
+    body.setAttribute("focus_comment", "");
+}
+
+function outFocusCommentInput() {
+    event.preventDefault();
+    let body = document.getElementsByTagName("body")[0];
+    body.removeAttribute("focus_comment");
+}
+
+function showComments() {
+    event.preventDefault();
+    let parent = document.getElementById("post " + event.srcElement.id);
+    if (parent.hasAttribute("show_comments")) {
+        parent.removeAttribute("show_comments");
+    } else {
+        parent.setAttribute("show_comments", "");
+    }
 }
