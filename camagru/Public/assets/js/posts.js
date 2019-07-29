be774870a3f2d9eacb2d88cf47b6971ec7018554
malallai <malallai@student.42.fr>
2019-07-29 16:45:32 +0200

    78 files changed, 5537 insertions(+), 310 deletions(-)

--- camagru/Public/assets/js/posts.js
+++ camagru/Public/assets/js/posts.js
@@ -7,6 +7,7 @@ function like() {
         url: url,
         type: 'POST',
         success: function(msg) {
+            console.log(msg);
             if (msg === 1) {
                 post.className = "far fa-heart";
             } else if (msg === 2) {
