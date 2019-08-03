a43e08c100cce37e9d4bd9894cd89b465ffdb425
malallai <malallai@student.42.fr>
2019-08-04 00:43:56 +0200

    83 files changed, 7133 insertions(+), 310 deletions(-)

--- camagru/test.php
+++ camagru/test.php
@@ -34,9 +34,14 @@ if (isset($_POST['submit'])) {
         $fianl_file = compress($source_img, $destination_url, 50);
 
         $file_url = 'https://camagru.malallai.fr/uploads/demo.jpg';
+        header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-        header("Content-Transfer-Encoding: Binary");
-        header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");
+        header('Content-Disposition: attachment; filename="'.basename($file_url).'"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file_url));
+        flush(); // Flush system output buffer
         readfile($file_url);
 
         $error = "Image Compressed successfully";
