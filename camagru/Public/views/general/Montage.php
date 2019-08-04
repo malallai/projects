e126cbbcf635bf02f5cc50f57db4054df0bd55dc
malallai <malallai@student.42.fr>
2019-08-04 17:38:24 +0200

    85 files changed, 6724 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Montage.php
+++ camagru/Public/views/general/Montage.php
@@ -3,7 +3,27 @@
 <div class="montage-row">
     <div id="main-row" class="row main-row">
         <div class="camera">
-            <video id="video" width="1280" height="720" autoplay></video>
+            <div class="video">
+                <video id="video" width="1280" height="720" autoplay></video>
+                <div class="details">
+                    <div class="row">
+                        <div class="content">
+                            <span class="like-count">
+                                <span><?= $likes ?></span>
+                                <i class="far fa-heart"></i>
+                            </span>
+                            <span class="comments-count">
+                                <span><?= $comments ?></span>
+                                <i class="far fa-comment-alt"></i>
+                            </span>
+                            <span class="delete-post">
+                                <span>&#160;</span>
+                                <a href="#" onclick="deletePost()"><i id="<?= $post['id'] ?>" class="fas fa-trash"></i></a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <canvas id="render" width="1280" height="720"></canvas>
         </div>
         <div class="filters">
