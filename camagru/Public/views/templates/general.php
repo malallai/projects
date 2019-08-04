8e465f22e15a413378491089438c30f0beeec4d1
malallai <malallai@student.42.fr>
2019-08-04 18:39:16 +0200

    85 files changed, 6731 insertions(+), 310 deletions(-)

--- camagru/Public/views/templates/general.php
+++ camagru/Public/views/templates/general.php
@@ -9,7 +9,6 @@
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src='/Public/assets/js/camagru.js'></script>
     <title>Camagru</title>
