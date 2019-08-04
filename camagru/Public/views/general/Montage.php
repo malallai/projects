b1e67c3946f80aba4496912326b35c4a5de38367
malallai <malallai@student.42.fr>
2019-08-04 15:58:38 +0200

    83 files changed, 6526 insertions(+), 310 deletions(-)

--- camagru/Public/views/general/Montage.php
+++ camagru/Public/views/general/Montage.php
@@ -1,1 +1,17 @@
-<?php
+<link href="/Public/assets/css/home.css" rel="stylesheet">
+<script src='/Public/assets/js/posts.js'></script>
+<div class="home-row">
+    <div id="posts-row" class="row posts-row">
+        <div class="posts">
+        </div>
+    </div>
+    <div id="users-row" class="row users-row">
+        <div class="content">
+            <div class="row-title">
+                <p>Utilisateurs</p>
+            </div>
+            <div class="users">
+            </div>
+        </div>
+    </div>
+</div>
