385db00ec945ca60a049d918d44ccee5303535ec
malallai <malallai@student.42.fr>
2019-08-04 15:23:29 +0200

    83 files changed, 6640 insertions(+), 310 deletions(-)

--- camagru/test.php
+++ camagru/test.php
@@ -43,7 +43,7 @@ if (isset($_POST['submit'])) {
 
         $final_file = compress($source_img, $destination_url, 50);
 
-        $file_url = '/uploads/demo.jpg';
+        $file_url = 'uploads/demo.jpg';
         header('Location: /test.php?dl='.$file_url);
         die();
 
