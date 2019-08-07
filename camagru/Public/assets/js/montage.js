446d4bc01e55fd4304d9a806807f49c7065eb3b6
malallai <malallai@student.42.fr>
2019-08-07 19:49:01 +0200

    88 files changed, 7097 insertions(+), 310 deletions(-)

--- camagru/Public/assets/js/montage.js
+++ camagru/Public/assets/js/montage.js
@@ -19,13 +19,58 @@ function montageReady() {
             if (filter) filter.removeAttribute("selected");
             filter = items;
             filter.setAttribute("selected", "");
+            updateFilter();
         });
     }
 
     setupCamera();
 }
 
- function cameraReady() {
+function reloadImage() {
+    let temp = document.getElementsByClassName("pics")[0].children[0].children[0].children[0].src;
+    context.drawImage(temp, 0, 0, canvas.width, canvas.height);
+}
+
+function turn_gray(my_context) {
+    var imageData = my_context.getImageData(0, 0, canvas.width, canvas.height);
+    for (j = 0; j < imageData.height; j++) {
+        for (i = 0; i < imageData.width; i++) {
+            var index = (j * 4) * imageData.width + (i * 4);
+            var red = imageData.data[index];
+            var green = imageData.data[index + 1];
+            var blue = imageData.data[index + 2];
+            var average = (red + green + blue) / 3;
+            imageData.data[index] = average;
+            imageData.data[index + 1] = average;
+            imageData.data[index + 2] = average;
+        }
+    }
+    my_context.putImageData(imageData, 0, 0);
+}
+
+function updateFilter() {
+    if (filter) {
+        switch (filter.children.id) {
+            case "void":
+                break;
+            case "sepia":
+                reloadImage();
+                turn_gray(context);
+                break;
+            case "gray":
+                reloadImage();
+                turn_gray(context);
+                break;
+            case "42":
+                let img = new Image();
+                img.src = filter.children[0].children[1].src;
+                img.onload = function(){
+                    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+                };
+                break;
+            default: break;
+        }
+    }
 }
 
 function switchDevice() {
@@ -39,6 +84,7 @@ function switchDevice() {
 function takePicture() {
     event.preventDefault();
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    document.getElementsByClassName("pics")[0].children[0].children[0].children[0].src = context.src;
 }
 
 function setupCamera() {
