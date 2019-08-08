8f5ecad41a49303d14217ee1618e6b7dad59a6f9
malallai <malallai@student.42.fr>
2019-08-08 18:32:02 +0200

    89 files changed, 7175 insertions(+), 310 deletions(-)

--- camagru/Public/assets/js/montage.js
+++ camagru/Public/assets/js/montage.js
@@ -24,6 +24,19 @@ function montageReady() {
         });
     }
 
+    canvas.addEventListener("mousedown", getPosition, false);
+    function getPosition(event) {
+        var x = event.x;
+        var y = event.y;
+
+        var canvas = document.getElementById("canvas");
+
+        x -= canvas.offsetLeft;
+        y -= canvas.offsetTop;
+
+        alert("x:" + x + " y:" + y);
+    }
+
     setupCamera();
 }
 
