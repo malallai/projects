3c1a69ff28865e1c365e6919c8b3670f58ccec04
malallai <malallai@student.42.fr>
2019-08-12 18:39:24 +0200

    2 files changed, 19 insertions(+), 19 deletions(-)

--- camagru/Pages/DevPage.php
+++ camagru/Pages/DevPage.php
@@ -26,7 +26,7 @@ class DevPage extends Page {
     }
 
     public function debug() {
-        header("Content-type: image/jpeg");
+        header("Content-type: text/plain");
         Session::startSession();
         var_dump($_SESSION);
         echo base64_decode($_SESSION['img']);
