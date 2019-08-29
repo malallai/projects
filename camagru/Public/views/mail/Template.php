c909ea8a9f10d12d87e72eff748f747c48d95323
malallai <malallai@student.42.fr>
2019-08-29 12:34:28 +0200

    101 files changed, 7729 insertions(+), 310 deletions(-)

--- camagru/Public/views/mail/Template.php
+++ camagru/Public/views/mail/Template.php
@@ -5,43 +5,26 @@ $mail_content = "Test";?>
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <style type="text/css">
-        body {margin: 0; padding: 0; min-width: 100%!important;}
-        .content {width: 100%; max-width: 600px;}
+    <style>
+        .mail-body { width: 100%; } .mail-body-content { width: 75%; margin-left: auto; margin-right: auto; color: black; } hr { width: 100%; border: none; height: 1px; background-color: rgb(153, 153, 153, .25); } h1 { text-transform: uppercase; text-align: center; } .object { text-align: center; color: #999; font-weight: bold; } .foot { text-align: center; font-size: 10px; color: #999; }
     </style>
 </head>
 
 <body>
-<!--    <style>-->
-<!--        .mail-body { width: 100%; } .mail-body-content { width: 75%; margin-left: auto; margin-right: auto; color: black; } hr { width: 100%; border: none; height: 1px; background-color: rgb(153, 153, 153, .25); } h1 { text-transform: uppercase; text-align: center; } .object { text-align: center; color: #999; font-weight: bold; } .foot { text-align: center; font-size: 10px; color: #999; }-->
-<!--    </style>-->
-<!--    <div class="mail-body">-->
-<!--        <div class="mail-body-content">-->
-<!--            <div class="head">-->
-<!--                <h1>Camagru</h1>-->
-<!--                <p class="object">--><?//= $object ?><!--</p>-->
-<!--            </div>-->
-<!--            <hr>-->
-<!--            <div class="content">--><?//= $mail_content ?><!--</div>-->
-<!--            <hr>-->
-<!--            <div class="foot">-->
-<!--                <p class="cop">Camagru - malallai 2019</p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-    <table width="100%" bgcolor="#f6f8f1" border="0" cellpadding="0" cellspacing="0">
-        <tr>
-            <td>
-                <table class="content" align="center" cellpadding="0" cellspacing="0" border="0">
-                    <tr>
-                        <td>
-                            Hello!
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+    <div class="mail-body">
+        <div class="mail-body-content">
+            <div class="head">
+                <h1>Camagru</h1>
+                <p class="object"><?= $object ?></p>
+            </div>
+            <hr>
+            <div class="content"><?= $mail_content ?></div>
+            <hr>
+            <div class="foot">
+                <p class="cop">Camagru - malallai 2019</p>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
