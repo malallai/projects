f02ff71edced4af096d4a4fba5688f56d163c031
malallai <malallai@student.42.fr>
2019-08-29 13:02:46 +0200

    101 files changed, 7715 insertions(+), 310 deletions(-)

--- camagru/config/Setup.php
+++ camagru/config/Setup.php
@@ -1,4 +1,11 @@
+<style>
+    form {
+        display: flex;
+        max-width: 50vw;
+        margin: 0 auto;
+    }
+</style>
 <form action="/setup" method="post">
     <input type="hidden" name="token" value="<?= $token ?>">
-    <input type="submit" name="submit" value="Setup Camagru ?">
+    <input class="button" type="submit" name="submit" value="Setup Camagru ?">
 </form>
