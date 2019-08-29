3ab5c966fd24add21be2dad3a75219ed4290dcfc
malallai <malallai@student.42.fr>
2019-08-29 15:32:20 +0200

    102 files changed, 7849 insertions(+), 310 deletions(-)

--- camagru/Public/views/templates/landing.php
+++ camagru/Public/views/templates/landing.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <meta name="author" content="malallai">
     <link rel="icon" href="/Public/assets/ico/favicon.ico">
-    <link href="/Public/assets/css/landing.css" rel="stylesheet">
     <link href="/Public/assets/css/general.css" rel="stylesheet">
+    <link href="/Public/assets/css/landing.css" rel="stylesheet">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
