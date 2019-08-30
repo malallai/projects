147488794c858573980e524651160acc388ec370
malallai <malallai@student.42.fr>
2019-08-30 16:07:02 +0200

    101 files changed, 7781 insertions(+), 310 deletions(-)

--- camagru/config/Setup.php
+++ camagru/config/Setup.php
@@ -1,14 +1,3 @@
-<style>
-    .form {
-        display: flex;
-        max-width: 500px;
-        margin: 0 auto;
-    }
-    form {
-        margin: auto;
-        width: 50%;
-    }
-</style>
 <div class="form">
     <form action="/setup" method="post">
         <input type="hidden" name="token" value="<?= $token ?>">
