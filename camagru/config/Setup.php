b0b7ba62beab7eacd9f591baad9b856eddaa9b02
malallai <malallai@student.42.fr>
2019-08-30 16:09:08 +0200

    101 files changed, 7797 insertions(+), 310 deletions(-)

--- camagru/config/Setup.php
+++ camagru/config/Setup.php
@@ -3,11 +3,11 @@
         width: 25%;
     }
 </style>
+<div class="infos">
+    <h1 class="title">Camagru</h1>
+    <p>Configurer Camagru ?</p>
+</div>
 <div class="form">
-    <div class="infos">
-        <h1 class="title">Camagru</h1>
-        <p>Configurer Camagru ?</p>
-    </div>
     <form action="/setup" method="post">
         <input type="hidden" name="token" value="<?= $token ?>">
         <input class="c-button-input button-light-blue" type="submit" name="submit" value="Ok">
