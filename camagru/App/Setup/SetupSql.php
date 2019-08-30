7f81059d7df18dad9cfaf0ee5f7bb2611e43f569
malallai <malallai@student.42.fr>
2019-08-30 17:42:48 +0200

    101 files changed, 7772 insertions(+), 310 deletions(-)

--- camagru/App/Setup/SetupSql.php
+++ camagru/App/Setup/SetupSql.php
@@ -46,28 +46,28 @@ class SetupSql extends Sql {
             "CREATE TABLE pwd_reset (
             user_id INT(11) UNSIGNED NOT NULL,
             token VARCHAR(255),
-            FOREIGN KEY (user_id) REFERENCES users(id))",
+            FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)",
             "CREATE TABLE posts (
             id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             user_id INT(11) UNSIGNED NOT NULL,
-            FOREIGN KEY (user_id) REFERENCES users(id),
+            FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
             image_path VARCHAR(255) NOT NULL,
             date TIMESTAMP DEFAULT current_timestamp())",
             "CREATE TABLE likes (
             post_id INT(11) UNSIGNED NOT NULL,
-            FOREIGN KEY (post_id) REFERENCES posts(id),
+            FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE,
             user_id INT(11) UNSIGNED NOT NULL,
-            FOREIGN KEY (user_id) REFERENCES users(id))",
+            FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)",
             "CREATE TABLE comments (
             post_id INT(11) UNSIGNED NOT NULL,
-            FOREIGN KEY (post_id) REFERENCES posts(id),
+            FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE,
             user_id INT(11) UNSIGNED NOT NULL,
-            FOREIGN KEY (user_id) REFERENCES users(id),
+            FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
             comment VARCHAR(255) NOT NULL,
             date TIMESTAMP DEFAULT current_timestamp())",
             "CREATE TABLE timer (
             user_id INT(11) UNSIGNED NOT NULL,
-            FOREIGN KEY (user_id) REFERENCES users(id),
+            FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
             last_comment TIMESTAMP DEFAULT current_timestamp())"
         ];
         foreach ($queries as $query) {
